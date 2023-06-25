0f191c5b0c48a20fb59607f7852a6983c816ae6b
Robert Balicki <robert.balicki@gmail.com>
2023-06-25 13:50:06 -0400

    remove outdated demo

--- demos/isograph-demo/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // To enable this flag, add code like
-  // [this](https://github.com/facebook/relay/blob/c0cc17a07e1f0c01f3e5c564eed50b5a30f4228f/packages/react-relay/relay-hooks/useEntryPointLoader.js#L156-L189)
-  // to useCachedPrecommitValue
-  reactStrictMode: false,
-}
-
-module.exports = nextConfig
--- demos/isograph-demo/src/network/index.ts
+++
@@ -1,8 +0,0 @@
-export const network = <T>(queryText: string, variables: Object): Promise<T> =>
-  fetch("http://localhost:4000/graphql", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ query: queryText, variables }),
-  }).then((response) => response.json());
