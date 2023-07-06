48515fdce8137279500b83c952c77b7d71268d1e
Robert Balicki <robert.balicki@gmail.com>
2023-07-06 15:04:00 +0800

    fix deploys for github demo

--- demos/github-demo/next.config.js
+++ demos/github-demo/next.config.js
@@ -8,6 +8,9 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  images: {
+    unoptimized: true,
+  },
 }
 
 module.exports = nextConfig
--- demos/github-demo/scripts/deploy.sh
+++ demos/github-demo/scripts/deploy.sh
@@ -9,4 +9,4 @@ cd ./out
 aws s3 rm s3://rb-pw-front/isograph-demo --recursive
 aws s3 cp . s3://rb-pw-front/isograph-demo --acl public-read --recursive
 aws s3 cp ./index.html s3://rb-pw-front/isograph-demo --acl public-read --cache-control max-age=0,no-cache --metadata-directive REPLACE
-aws cloudfront create-invalidation --distribution-id E3QXR86BT07VQ8 --paths /*
+aws cloudfront create-invalidation --distribution-id E3QXR86BT07VQ8 --paths "/*"
