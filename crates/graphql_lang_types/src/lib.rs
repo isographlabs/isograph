b7a83969c519733cb126480412712ac310348603
Robert Balicki <robert.balicki@gmail.com>
2023-08-24 15:03:17 -0400

    rename files for better sanity

--- crates/graphql_lang_types/src/lib.rs
+++ crates/graphql_lang_types/src/lib.rs
@@ -1,11 +1,11 @@
 mod directive;
-mod schema_def;
+mod graphql_sdl;
 mod type_annotation;
 mod value;
 mod write;
 
 pub use directive::*;
-pub use schema_def::*;
+pub use graphql_sdl::*;
 pub use type_annotation::*;
 pub use value::*;
 pub use write::*;
--- crates/isograph_schema/src/lib.rs
+++ crates/isograph_schema/src/lib.rs
@@ -1,11 +1,11 @@
 mod merged;
 mod process_resolver_declaration;
 mod process_type_definition;
-mod schema_types;
+mod schema;
 mod validate_schema;
 
 pub use merged::*;
 pub use process_resolver_declaration::*;
 pub use process_type_definition::*;
-pub use schema_types::*;
+pub use schema::*;
 pub use validate_schema::*;
