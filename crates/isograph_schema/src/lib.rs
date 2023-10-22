56dc08b2653d83bd7a6ed329543404821815c33e
Robert Balicki <robert.balicki@gmail.com>
2023-10-22 08:09:40 -0400

    rename file

--- crates/isograph_schema/src/lib.rs
+++ crates/isograph_schema/src/lib.rs
@@ -1,12 +1,12 @@
+mod isograph_schema;
 mod merged;
 mod process_resolver_declaration;
 mod process_type_definition;
 pub(crate) mod refetched_paths;
-mod schema;
 mod validate_schema;
 
+pub use isograph_schema::*;
 pub use merged::*;
 pub use process_resolver_declaration::*;
 pub use process_type_definition::*;
-pub use schema::*;
 pub use validate_schema::*;
