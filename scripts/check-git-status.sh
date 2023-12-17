2ed9f55441b0094182930a4ef0bc215e1c31da69
Robert Balicki <robert.balicki@gmail.com>
2023-12-17 12:52:08 -0500

    add ci

---
+++ scripts/check-git-status.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [ -z "$(git status --porcelain)" ]; then
+  # Working directory clean
+  exit 0
+else
+  echo "Detected changes in the working directory:"
+  git --no-pager diff --stat
+  exit 1
+fi
