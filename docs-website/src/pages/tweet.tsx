773546b9a9b6c7ae716d97cac6c9f1b1a0a7111a
Robert Balicki <robert.balicki@gmail.com>
2024-02-15 13:03:12 -0500

    add tweet page

---
+++ docs-website/src/pages/tweet.tsx
@@ -0,0 +1,26 @@
+import Layout from '@theme/Layout';
+
+import { TwitterTweetEmbed } from 'react-twitter-embed';
+
+export default function Home(): JSX.Element {
+  return (
+    <Layout
+      title={`Isograph, the UI framework for teams that move fast without breaking things`}
+      description="Isograph, the UI framework for teams that move fast without breaking things"
+    >
+      <main>
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'center',
+          }}
+        >
+          <div style={{ minWidth: 500 }}>
+            <TwitterTweetEmbed tweetId={'1758185952759992507'} />
+          </div>
+        </div>
+      </main>
+    </Layout>
+  );
+}
