dad145e2fd4080f5f195ec7cc29667f9c6c97262
Robert Balicki <robert.balicki@gmail.com>
2024-02-20 11:46:43 -0500

    Improve typing for EntrypointReader
    * This component is pretty useless, since useResult will never actually
      suspend (since @component's cannot currently suspend.)
    * Is this API worth having?

--- libs/isograph-react/src/EntrypointReader.tsx
+++ libs/isograph-react/src/EntrypointReader.tsx
@@ -2,21 +2,21 @@ import * as React from 'react';
 import {
   ExtractReadFromStore,
   ExtractResolverProps,
-  ExtractResolverResult,
   IsographEntrypoint,
   type FragmentReference,
   useResult,
 } from './index';
 
 export function EntrypointReader<
-  TEntrypoint extends IsographEntrypoint<any, any, any>,
+  TEntrypoint extends IsographEntrypoint<any, any, React.FC<any>>,
 >(props: {
   queryReference: FragmentReference<
     ExtractReadFromStore<TEntrypoint>,
     ExtractResolverProps<TEntrypoint>,
-    ExtractResolverResult<TEntrypoint>
+    React.FC<any>
   >;
+  additionalProps?: any | void;
 }): ReturnType<React.FC<any>> {
   const Component = useResult(props.queryReference);
-  return <Component />;
+  return <Component {...props.additionalProps} />;
 }
