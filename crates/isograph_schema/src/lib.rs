9294e976c8fb506ec8bb12dc902d405d2d9b3ab6
Robert Balicki <robert.balicki@gmail.com>
2024-03-25 02:23:33 +0900

    rename file

--- crates/isograph_schema/src/lib.rs
+++ crates/isograph_schema/src/lib.rs
@@ -1,8 +1,8 @@
 mod add_fields_to_subtypes;
 mod argument_map;
+mod create_merged_selection_set;
 mod expose_field_directive;
 mod isograph_schema;
-mod merged;
 mod process_client_field_declaration;
 mod process_type_definition;
 pub(crate) mod refetched_paths;
@@ -13,9 +13,9 @@ mod validate_schema;
 
 use argument_map::*;
 
+pub use create_merged_selection_set::*;
 pub use expose_field_directive::*;
 pub use isograph_schema::*;
-pub use merged::*;
 pub use process_client_field_declaration::*;
 pub use process_type_definition::*;
 use root_types::*;
