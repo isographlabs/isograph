1664b09cd8749a92094879a7e6ce3559cb101d91
Robert Balicki <robert.balicki@gmail.com>
2024-06-22 22:24:54 -0600

    refactor: extract two functions

--- crates/graphql_artifact_generation/src/reader_ast.rs
+++ crates/graphql_artifact_generation/src/reader_ast.rs
@@ -1,10 +1,11 @@
 use std::collections::{BTreeMap, HashSet};
 
 use common_lang_types::{SelectableFieldName, WithSpan};
-use isograph_lang_types::{RefetchQueryIndex, Selection, ServerFieldSelection};
+use isograph_lang_types::{ClientFieldId, RefetchQueryIndex, Selection, ServerFieldSelection};
 use isograph_schema::{
     into_name_and_arguments, ArgumentKeyAndValue, ClientFieldVariant, FieldDefinitionLocation,
-    NameAndArguments, PathToRefetchField, RootRefetchedPath, ValidatedClientField, ValidatedSchema,
+    NameAndArguments, PathToRefetchField, RootRefetchedPath, ValidatedClientField,
+    ValidatedLinkedFieldSelection, ValidatedScalarFieldSelection, ValidatedSchema,
     ValidatedSelection,
 };
 
@@ -18,7 +19,7 @@ fn generate_reader_ast_node(
     selection: &WithSpan<ValidatedSelection>,
     schema: &ValidatedSchema,
     indentation_level: u8,
-    nested_client_field_imports: &mut ReaderImports,
+    reader_imports: &mut ReaderImports,
     // TODO use this to generate usedRefetchQueries
     root_refetched_paths: &BTreeMap<PathToRefetchField, RootRefetchedPath>,
     path: &mut Vec<NameAndArguments>,
@@ -26,158 +27,228 @@ fn generate_reader_ast_node(
     match &selection.item {
         Selection::ServerField(field) => match field {
             ServerFieldSelection::ScalarField(scalar_field) => {
-                let field_name = scalar_field.name.item;
-
                 match scalar_field.associated_data.location {
                     FieldDefinitionLocation::Server(_) => {
-                        let alias = scalar_field
-                            .reader_alias
-                            .map(|x| format!("\"{}\"", x.item))
-                            .unwrap_or("null".to_string());
-                        let arguments = get_serialized_field_arguments(
-                            &scalar_field
-                                .arguments
-                                .iter()
-                                .map(|x| x.item.clone())
-                                .collect::<Vec<_>>(),
-                            indentation_level + 1,
-                        );
-
-                        let indent_1 = "  ".repeat(indentation_level as usize);
-                        let indent_2 = "  ".repeat((indentation_level + 1) as usize);
-
-                        format!(
-                            "{indent_1}{{\n\
-                            {indent_2}kind: \"Scalar\",\n\
-                            {indent_2}fieldName: \"{field_name}\",\n\
-                            {indent_2}alias: {alias},\n\
-                            {indent_2}arguments: {arguments},\n\
-                            {indent_1}}},\n",
-                        )
+                        server_defined_scalar_field_ast_node(scalar_field, indentation_level)
                     }
                     FieldDefinitionLocation::Client(client_field_id) => {
-                        // This field is a client field, so we need to look up the field in the
-                        // schema.
-                        let alias = scalar_field.name_or_alias().item;
-                        let nested_client_field = schema.client_field(client_field_id);
-                        let arguments = get_serialized_field_arguments(
-                            &scalar_field
-                                .arguments
-                                .iter()
-                                .map(|x| x.item.clone())
-                                .collect::<Vec<_>>(),
-                            indentation_level + 1,
-                        );
-                        let indent_1 = "  ".repeat(indentation_level as usize);
-                        let indent_2 = "  ".repeat((indentation_level + 1) as usize);
-
-                        let paths_to_refetch_field_in_client_field =
-                            refetched_paths_for_client_field(nested_client_field, schema, path);
-
-                        let nested_refetch_queries = get_nested_refetch_query_text(
-                            &root_refetched_paths,
-                            &paths_to_refetch_field_in_client_field,
-                        );
-
-                        // This is indicative of poor data modeling.
-                        match nested_client_field.variant {
-                            ClientFieldVariant::ImperativelyLoadedField(ref s) => {
-                                let reader_artifact_import_name = format!(
-                                    "{}__refetch_reader",
-                                    nested_client_field.type_and_field.underscore_separated()
-                                );
-
-                                {
-                                    let artifact_file_type =
-                                        ResolverReaderOrRefetchResolver::RefetchReader;
-                                    nested_client_field_imports.insert(
-                                        nested_client_field.type_and_field,
-                                        artifact_file_type,
-                                    );
-                                };
-                                let refetch_query_index = find_imperatively_fetchable_query_index(
-                                    root_refetched_paths,
-                                    path,
-                                    s.client_field_scalar_selection_name.into(),
-                                )
-                                .0;
-
-                                format!(
-                                    "{indent_1}{{\n\
-                                    {indent_2}kind: \"ImperativelyLoadedField\",\n\
-                                    {indent_2}alias: \"{alias}\",\n\
-                                    {indent_2}readerArtifact: {reader_artifact_import_name},\n\
-                                    {indent_2}refetchQuery: {refetch_query_index},\n\
-                                    {indent_1}}},\n",
-                                )
-                            }
-                            ClientFieldVariant::UserWritten(_) => {
-                                let reader_artifact_import_name = format!(
-                                    "{}__resolver_reader",
-                                    nested_client_field.type_and_field.underscore_separated()
-                                );
-
-                                nested_client_field_imports.insert(
-                                    nested_client_field.type_and_field,
-                                    ResolverReaderOrRefetchResolver::ResolverReader,
-                                );
-
-                                format!(
-                                    "{indent_1}{{\n\
-                                    {indent_2}kind: \"Resolver\",\n\
-                                    {indent_2}alias: \"{alias}\",\n\
-                                    {indent_2}arguments: {arguments},\n\
-                                    {indent_2}readerArtifact: {reader_artifact_import_name},\n\
-                                    {indent_2}usedRefetchQueries: {nested_refetch_queries},\n\
-                                    {indent_1}}},\n",
-                                )
-                            }
-                        }
+                        scalar_client_defined_field_ast_node(
+                            scalar_field,
+                            schema,
+                            client_field_id,
+                            indentation_level,
+                            path,
+                            root_refetched_paths,
+                            reader_imports,
+                        )
                     }
                 }
             }
             ServerFieldSelection::LinkedField(linked_field) => {
-                let name = linked_field.name.item;
-                let alias = linked_field
-                    .reader_alias
-                    .map(|x| format!("\"{}\"", x.item))
-                    .unwrap_or("null".to_string());
-
                 path.push(into_name_and_arguments(&linked_field));
 
                 let inner_reader_ast = generate_reader_ast_with_path(
                     schema,
                     &linked_field.selection_set,
                     indentation_level + 1,
-                    nested_client_field_imports,
+                    reader_imports,
                     root_refetched_paths,
                     path,
                 );
 
                 path.pop();
 
-                let arguments = get_serialized_field_arguments(
-                    &linked_field
-                        .arguments
-                        .iter()
-                        .map(|x| x.item.clone())
-                        .collect::<Vec<_>>(),
-                    indentation_level + 1,
-                );
-                let indent_1 = "  ".repeat(indentation_level as usize);
-                let indent_2 = "  ".repeat((indentation_level + 1) as usize);
-                format!(
-                    "{indent_1}{{\n\
-                    {indent_2}kind: \"Linked\",\n\
-                    {indent_2}fieldName: \"{name}\",\n\
-                    {indent_2}alias: {alias},\n\
-                    {indent_2}arguments: {arguments},\n\
-                    {indent_2}selections: {inner_reader_ast},\n\
-                    {indent_1}}},\n",
-                )
+                linked_field_ast_node(linked_field, indentation_level, inner_reader_ast)
             }
         },
     }
+}
+
+fn linked_field_ast_node(
+    linked_field: &ValidatedLinkedFieldSelection,
+    indentation_level: u8,
+    inner_reader_ast: ReaderAst,
+) -> String {
+    let name = linked_field.name.item;
+    let alias = linked_field
+        .reader_alias
+        .map(|x| format!("\"{}\"", x.item))
+        .unwrap_or("null".to_string());
+
+    let arguments = get_serialized_field_arguments(
+        &linked_field
+            .arguments
+            .iter()
+            .map(|x| x.item.clone())
+            .collect::<Vec<_>>(),
+        indentation_level + 1,
+    );
+    let indent_1 = "  ".repeat(indentation_level as usize);
+    let indent_2 = "  ".repeat((indentation_level + 1) as usize);
+    format!(
+        "{indent_1}{{\n\
+        {indent_2}kind: \"Linked\",\n\
+        {indent_2}fieldName: \"{name}\",\n\
+        {indent_2}alias: {alias},\n\
+        {indent_2}arguments: {arguments},\n\
+        {indent_2}selections: {inner_reader_ast},\n\
+        {indent_1}}},\n",
+    )
+}
+
+fn scalar_client_defined_field_ast_node(
+    scalar_field_selection: &ValidatedScalarFieldSelection,
+    schema: &ValidatedSchema,
+    client_field_id: ClientFieldId,
+    indentation_level: u8,
+    path: &mut Vec<NameAndArguments>,
+    root_refetched_paths: &BTreeMap<PathToRefetchField, RootRefetchedPath>,
+    reader_imports: &mut ReaderImports,
+) -> String {
+    // This field is a client field, so we need to look up the field in the
+    // schema.
+    let nested_client_field = schema.client_field(client_field_id);
+
+    // This is indicative of poor data modeling.
+    match nested_client_field.variant {
+        ClientFieldVariant::ImperativelyLoadedField(_) => imperatively_loaded_variant_ast_node(
+            nested_client_field,
+            reader_imports,
+            root_refetched_paths,
+            path,
+            indentation_level,
+            scalar_field_selection,
+        ),
+        ClientFieldVariant::UserWritten(_) => user_written_variant_ast_node(
+            scalar_field_selection,
+            indentation_level,
+            nested_client_field,
+            schema,
+            path,
+            root_refetched_paths,
+            reader_imports,
+        ),
+    }
+}
+
+fn user_written_variant_ast_node(
+    scalar_field_selection: &ValidatedScalarFieldSelection,
+    indentation_level: u8,
+    nested_client_field: &ValidatedClientField,
+    schema: &ValidatedSchema,
+    path: &mut Vec<NameAndArguments>,
+    root_refetched_paths: &BTreeMap<PathToRefetchField, RootRefetchedPath>,
+    reader_imports: &mut ReaderImports,
+) -> String {
+    let alias = scalar_field_selection.name_or_alias().item;
+    let indent_1 = "  ".repeat(indentation_level as usize);
+    let indent_2 = "  ".repeat((indentation_level + 1) as usize);
+    let paths_to_refetch_field_in_client_field =
+        refetched_paths_for_client_field(nested_client_field, schema, path);
+
+    let nested_refetch_queries = get_nested_refetch_query_text(
+        &root_refetched_paths,
+        &paths_to_refetch_field_in_client_field,
+    );
+
+    let arguments = get_serialized_field_arguments(
+        &scalar_field_selection
+            .arguments
+            .iter()
+            // TODO we shouldn't need to clone here
+            .map(|x| x.item.clone())
+            .collect::<Vec<_>>(),
+        indentation_level + 1,
+    );
+
+    let reader_artifact_import_name = format!(
+        "{}__resolver_reader",
+        nested_client_field.type_and_field.underscore_separated()
+    );
+
+    reader_imports.insert(
+        nested_client_field.type_and_field,
+        ResolverReaderOrRefetchResolver::ResolverReader,
+    );
+
+    format!(
+        "{indent_1}{{\n\
+        {indent_2}kind: \"Resolver\",\n\
+        {indent_2}alias: \"{alias}\",\n\
+        {indent_2}arguments: {arguments},\n\
+        {indent_2}readerArtifact: {reader_artifact_import_name},\n\
+        {indent_2}usedRefetchQueries: {nested_refetch_queries},\n\
+        {indent_1}}},\n",
+    )
+}
+
+fn imperatively_loaded_variant_ast_node(
+    nested_client_field: &ValidatedClientField,
+    reader_imports: &mut ReaderImports,
+    root_refetched_paths: &BTreeMap<PathToRefetchField, RootRefetchedPath>,
+    path: &mut Vec<NameAndArguments>,
+    indentation_level: u8,
+    scalar_field_selection: &ValidatedScalarFieldSelection,
+) -> String {
+    let alias = scalar_field_selection.name_or_alias().item;
+    let indent_1 = "  ".repeat(indentation_level as usize);
+    let indent_2 = "  ".repeat((indentation_level + 1) as usize);
+
+    let reader_artifact_import_name = format!(
+        "{}__refetch_reader",
+        nested_client_field.type_and_field.underscore_separated()
+    );
+
+    {
+        let artifact_file_type = ResolverReaderOrRefetchResolver::RefetchReader;
+        reader_imports.insert(nested_client_field.type_and_field, artifact_file_type);
+    };
+    let refetch_query_index = find_imperatively_fetchable_query_index(
+        root_refetched_paths,
+        path,
+        scalar_field_selection.name.item.into(),
+    )
+    .0;
+
+    format!(
+        "{indent_1}{{\n\
+        {indent_2}kind: \"ImperativelyLoadedField\",\n\
+        {indent_2}alias: \"{alias}\",\n\
+        {indent_2}readerArtifact: {reader_artifact_import_name},\n\
+        {indent_2}refetchQuery: {refetch_query_index},\n\
+        {indent_1}}},\n",
+    )
+}
+
+fn server_defined_scalar_field_ast_node(
+    scalar_field: &ValidatedScalarFieldSelection,
+    indentation_level: u8,
+) -> String {
+    let field_name = scalar_field.name.item;
+    let alias = scalar_field
+        .reader_alias
+        .map(|x| format!("\"{}\"", x.item))
+        .unwrap_or("null".to_string());
+    let arguments = get_serialized_field_arguments(
+        &scalar_field
+            .arguments
+            .iter()
+            .map(|x| x.item.clone())
+            .collect::<Vec<_>>(),
+        indentation_level + 1,
+    );
+
+    let indent_1 = "  ".repeat(indentation_level as usize);
+    let indent_2 = "  ".repeat((indentation_level + 1) as usize);
+
+    format!(
+        "{indent_1}{{\n\
+        {indent_2}kind: \"Scalar\",\n\
+        {indent_2}fieldName: \"{field_name}\",\n\
+        {indent_2}alias: {alias},\n\
+        {indent_2}arguments: {arguments},\n\
+        {indent_1}}},\n",
+    )
 }
 
 fn generate_reader_ast_with_path<'schema>(
