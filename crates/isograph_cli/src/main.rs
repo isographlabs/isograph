c7d38e904a8f79a02609c83e820fe2bbd532b137
Edmondo Porcu <edmondo.porcu@gmail.com>
2024-08-03 17:26:53 +0000

    Making compile the default command of isograph cli

--- crates/isograph_cli/src/main.rs
+++ crates/isograph_cli/src/main.rs
@@ -6,28 +6,35 @@ use isograph_compiler::handle_watch_command;
 use isograph_config::create_config;
 use isograph_lsp::lsp_process_error::LSPProcessError;
 use isograph_lsp::server;
-use opt::CliOptions;
-use opt::CompilerCLIOptions;
-use opt::LSPCLIOptions;
+use opt::Command;
+use opt::CompileCommand;
+use opt::LSPCommand;
+use opt::Opt;
 use structopt::StructOpt;
 
 #[tokio::main]
 async fn main() {
-    let opt = CliOptions::from_args();
-    match opt {
-        CliOptions::Compile(compiler_options) => {
-            start_compiler(compiler_options).await;
+    let opt = Opt::from_args();
+    let command = opt.command.unwrap_or_else(|| {
+        Command::Compile(
+            opt.compile
+                .expect("Command not provided and compile options not set"),
+        )
+    });
+    match command {
+        Command::Compile(compile_command) => {
+            start_compiler(compile_command).await;
         }
-        CliOptions::LSP(lsp_options) => {
-            start_language_server(lsp_options).await.unwrap();
+        Command::LSP(lsp_command) => {
+            start_language_server(lsp_command).await.unwrap();
         }
     }
 }
 
-async fn start_compiler(opt: CompilerCLIOptions) {
-    let config = create_config(opt.config.unwrap_or("./isograph.config.json".into()));
+async fn start_compiler(compile_command: CompileCommand) {
+    let config = create_config(compile_command.config.unwrap_or("./isograph.config.json".into()));
 
-    if opt.watch {
+    if compile_command.watch {
         match handle_watch_command(config).await {
             Ok(res) => match res {
                 Ok(_) => {
@@ -55,9 +62,9 @@ async fn start_compiler(opt: CompilerCLIOptions) {
         std::process::exit(1);
     }
 }
-async fn start_language_server(lsp_options: LSPCLIOptions) -> Result<(), LSPProcessError> {
+async fn start_language_server(lsp_command: LSPCommand) -> Result<(), LSPProcessError> {
     let config = create_config(
-        lsp_options
+        lsp_command
             .config
             .unwrap_or("./isograph.config.json".into()),
     );
--- crates/isograph_cli/src/opt.rs
+++ crates/isograph_cli/src/opt.rs
@@ -1,15 +1,23 @@
 use std::path::PathBuf;
 
 use structopt::StructOpt;
+#[derive(Debug, StructOpt)]
+pub struct Opt {
+    #[clap(subcommand)]
+    pub command: Option<Command>,
+
+    #[clap(flatten)]
+    pub compile: Option<CompileCommand>,
+}
 
 #[derive(Debug, StructOpt)]
-pub enum CliOptions {
-    Compile(CompilerCLIOptions),
-    LSP(LSPCLIOptions),
+pub enum Command {
+    Compile(CompileCommand),
+    LSP(LSPCommand),
 }
 /// Compile
 #[derive(Debug, StructOpt)]
-pub(crate) struct CompilerCLIOptions {
+pub(crate) struct CompileCommand {
     #[structopt(long)]
     pub watch: bool,
 
@@ -21,7 +29,7 @@ pub(crate) struct CompilerCLIOptions {
 
 /// LSP
 #[derive(Debug, StructOpt)]
-pub(crate) struct LSPCLIOptions {
+pub(crate) struct LSPCommand {
     /// Compile using this config file. If not provided, searches for a config in
     /// package.json under the `isograph` key.
     #[structopt(long)]
