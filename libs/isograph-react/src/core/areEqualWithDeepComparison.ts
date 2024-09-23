7d4bae3eb33e1bc05c203a0547e2c04373137e2c
Patryk Wałach <35966385+PatrykWalach@users.noreply.github.com>
2024-09-24 00:01:19 +0200

    recycle old nodes (#117)
    Recycle old nodes when comparing fragment results. So, even if the entire object (`data`) isn't stable across renders, sub-objects may be (`data.user`) if that part of the tree is unchanged.

--- libs/isograph-react/src/core/areEqualWithDeepComparison.ts
+++ libs/isograph-react/src/core/areEqualWithDeepComparison.ts
@@ -1,38 +1,34 @@
 import type { ReaderAst, ReaderLinkedField, ReaderScalarField } from './reader';
-export function areEqualWithDeepComparison(
+export function mergeUsingReaderAst(
   field: ReaderScalarField | ReaderLinkedField,
   oldItem: unknown,
   newItem: unknown,
-): boolean {
+): unknown {
   if (newItem === null) {
-    return oldItem === null;
+    return oldItem === null ? oldItem : newItem;
   }
 
   if (newItem === undefined) {
-    return oldItem === undefined;
+    return oldItem === undefined ? oldItem : newItem;
   }
 
   if (Array.isArray(newItem)) {
     if (!Array.isArray(oldItem)) {
-      return false;
+      return newItem;
     }
 
-    return areEqualArraysWithDeepComparison(field, oldItem, newItem);
+    return mergeArraysUsingReaderAst(field, oldItem, newItem);
   }
 
   switch (field.kind) {
     case 'Scalar':
-      return newItem === oldItem;
+      return oldItem === newItem ? oldItem : newItem;
     case 'Linked':
       if (oldItem == null) {
-        return false;
+        return newItem;
       }
 
-      return areEqualObjectsWithDeepComparison(
-        field.selections,
-        oldItem,
-        newItem,
-      );
+      return mergeObjectsUsingReaderAst(field.selections, oldItem, newItem);
     default: {
       // Ensure we have covered all variants
       let _: never = field;
@@ -42,29 +38,34 @@ export function areEqualWithDeepComparison(
   }
 }
 
-export function areEqualArraysWithDeepComparison(
+export function mergeArraysUsingReaderAst(
   field: ReaderScalarField | ReaderLinkedField,
   oldItems: ReadonlyArray<unknown>,
-  newItems: ReadonlyArray<unknown>,
-): boolean {
+  newItems: Array<unknown>,
+): ReadonlyArray<unknown> {
   if (newItems.length !== oldItems.length) {
-    return false;
+    return newItems;
   }
 
+  let canRecycle = true;
   for (let i = 0; i < newItems.length; i++) {
-    if (!areEqualWithDeepComparison(field, oldItems[i], newItems[i])) {
-      return false;
+    const mergedItem = mergeUsingReaderAst(field, oldItems[i], newItems[i]);
+    if (mergedItem !== oldItems[i]) {
+      canRecycle = false;
+    } else {
+      newItems[i] = mergedItem;
     }
   }
 
-  return true;
+  return canRecycle ? oldItems : newItems;
 }
 
-export function areEqualObjectsWithDeepComparison(
+export function mergeObjectsUsingReaderAst(
   ast: ReaderAst<object>,
   oldItemObject: object,
   newItemObject: object,
-): boolean {
+): object {
+  let canRecycle = true;
   for (const field of ast) {
     switch (field.kind) {
       case 'Scalar':
@@ -74,8 +75,13 @@ export function areEqualObjectsWithDeepComparison(
         const oldValue = oldItemObject[key];
         // @ts-expect-error
         const newValue = newItemObject[key];
-        if (!areEqualWithDeepComparison(field, oldValue, newValue)) {
-          return false;
+
+        const mergedValue = mergeUsingReaderAst(field, oldValue, newValue);
+        if (mergedValue !== oldValue) {
+          canRecycle = false;
+        } else {
+          // @ts-expect-error
+          newItemObject[key] = mergedValue;
         }
         break;
       case 'Resolver': {
@@ -86,7 +92,7 @@ export function areEqualObjectsWithDeepComparison(
         const newValue = newItemObject[key];
 
         if (oldValue !== newValue) {
-          return false;
+          canRecycle = false;
         }
         break;
       }
@@ -102,5 +108,5 @@ export function areEqualObjectsWithDeepComparison(
     }
   }
 
-  return true;
+  return canRecycle ? oldItemObject : newItemObject;
 }
