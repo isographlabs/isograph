e60ef6b875fac6ac671352e8346efac0353122f2
Robert Balicki <robert.balicki@gmail.com>
2024-10-05 10:23:03 -0400

    try this

--- crates/isograph_compiler/src/batch_compile.rs
+++ crates/isograph_compiler/src/batch_compile.rs
@@ -110,11 +110,9 @@ pub fn compile_and_print(config: &CompilerConfig) -> Result<CompilationStats, Ba
 ///
 /// Isograph is currently written in a fairly sequential fashion, e.g.:
 ///
-/// ```no_run
 /// let result_1 = step_1()?;
 /// let result_2 = step_2()?;
 /// step_3(result_1, result_2)?;
-/// ```
 ///
 /// Where each step is completed before the next one starts. This has advantages:
 /// namely, it is easy to read. But, we most likely want to report all the errors
