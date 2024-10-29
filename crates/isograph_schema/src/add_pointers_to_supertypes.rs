2d80240da0e786b15764f8da5e4033461b13fa51
Patryk Wałach <35966385+PatrykWalach@users.noreply.github.com>
2024-10-29 20:09:20 +0100

    Generate pointers for subtypes on supertypes (#227)
    - generate `asConcreteType` fields on supertypes (e.g. `Node`) for each subtype (e.g. `User`)
    - if selected, these will currently generate invalid queries. Once we modify merged selection set generation to accommodate these, this will no longer be the case

---
+++ crates/isograph_schema/src/add_pointers_to_supertypes.rs
@@ -0,0 +1,66 @@
+use common_lang_types::{SelectableFieldName, Span, UnvalidatedTypeName, WithLocation, WithSpan};
+use graphql_lang_types::{GraphQLNamedTypeAnnotation, GraphQLTypeAnnotation};
+use intern::string_key::Intern;
+
+use crate::{
+    FieldType, ProcessTypeDefinitionError, ProcessTypeDefinitionResult, SchemaObject,
+    SchemaServerField, TypeRefinementMap, UnvalidatedSchema,
+};
+use common_lang_types::Location;
+impl UnvalidatedSchema {
+    /// For each supertype (e.g. Node), add the pointers for each subtype (e.g. User implements Node)
+    /// to supertype (e.g. creating Node.asUser).
+    pub fn add_pointers_to_supertypes(
+        &mut self,
+        subtype_to_supertype_map: &TypeRefinementMap,
+    ) -> ProcessTypeDefinitionResult<()> {
+        for (subtype_id, supertype_ids) in subtype_to_supertype_map {
+            let subtype: &SchemaObject = self.server_field_data.object(*subtype_id);
+
+            let field_name: SelectableFieldName = format!("as{}", subtype.name).intern().into();
+
+            let next_server_field_id = self.server_fields.len().into();
+
+            let associated_data: GraphQLTypeAnnotation<UnvalidatedTypeName> =
+                GraphQLTypeAnnotation::Named(GraphQLNamedTypeAnnotation(WithSpan {
+                    item: subtype.name.into(),
+                    span: Span::todo_generated(),
+                }));
+
+            let server_field = SchemaServerField {
+                description: Some(
+                    format!("A client poiter for the {} type.", subtype.name)
+                        .intern()
+                        .into(),
+                ),
+                id: next_server_field_id,
+                name: WithLocation::new(field_name, Location::generated()),
+                parent_type_id: subtype.id,
+                arguments: vec![],
+                associated_data,
+                is_discriminator: false,
+            };
+
+            self.server_fields.push(server_field);
+
+            for supertype_id in supertype_ids {
+                let supertype = self.server_field_data.object_mut(*supertype_id);
+
+                if supertype
+                    .encountered_fields
+                    .insert(field_name, FieldType::ServerField(next_server_field_id))
+                    .is_some()
+                {
+                    return Err(WithLocation::new(
+                        ProcessTypeDefinitionError::FieldExistsOnType {
+                            field_name,
+                            parent_type: supertype.name,
+                        },
+                        Location::generated(),
+                    ));
+                }
+            }
+        }
+        Ok(())
+    }
+}
