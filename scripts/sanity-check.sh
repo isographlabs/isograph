5dd48813ff6a8f7befb5d51f49ad9630bacda0d2
Patryk Wałach <35966385+PatrykWalach@users.noreply.github.com>
2024-11-02 16:52:21 +0100

    enable tsconfig project references

--- scripts/sanity-check.sh
+++ scripts/sanity-check.sh
@@ -2,7 +2,7 @@ pnpm format && \
   cargo clippy && \
   pnpm build-demos && \
   ./scripts/check-git-status.sh && \
-  pnpm -r compile && \
+  pnpm exec tsc --build && \
   pnpm test
 if [ $? -eq 0 ]; then
     echo OK
