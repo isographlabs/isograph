16bf47c554da27b304219feb38dd5bb9a62a3ecc
Patryk Wałach <35966385+PatrykWalach@users.noreply.github.com>
2024-11-09 18:27:41 +0100

    update sanity-check

--- scripts/sanity-check.sh
+++ scripts/sanity-check.sh
@@ -2,7 +2,7 @@ pnpm format && \
   cargo clippy && \
   pnpm build-demos && \
   ./scripts/check-git-status.sh && \
-  pnpm -r compile && \
+  pnpm compile && \
   pnpm test
 if [ $? -eq 0 ]; then
     echo OK
