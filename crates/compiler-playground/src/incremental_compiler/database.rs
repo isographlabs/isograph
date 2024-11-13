27746308b7c50d46214083fff5b5e03c3cf1306b
Robert Balicki <robert.balicki@gmail.com>
2024-11-13 10:00:48 -0500

    moar

--- crates/compiler-playground/src/incremental_compiler/database.rs
+++ crates/compiler-playground/src/incremental_compiler/database.rs
@@ -9,6 +9,7 @@ use string_key_newtype::string_key_newtype;
 pub struct Database {
     pub current_epoch: usize,
     pub nodes: HashMap<NodeId, DerivedNode>,
+    pub dependency_stack: Vec<Vec<(usize, Dependency)>>,
 }
 
 impl Database {
@@ -16,14 +17,15 @@ impl Database {
         &mut self,
         static_key: &'static str,
         param: TParam,
-        inner_fn: impl Fn(&mut Database, TParam) -> TOutput,
+        inner_fn: impl Fn(&mut Database, TParam) -> TOutput + 'static,
     ) -> TOutput
     where
-        TParam: Hash + Clone,
+        TParam: Hash + Clone + 'static,
         TOutput: AnyEq + Eq + Clone,
     {
         let node_id = NodeId::new(static_key, &param);
-        if self.nodes.contains_key(&node_id) {
+
+        let (value, time_calculated) = if self.nodes.contains_key(&node_id) {
             // Collect dependent nodes ids that might be changed
             let maybe_changed = self
                 .nodes
@@ -32,7 +34,7 @@ impl Database {
                 .dependencies
                 .iter()
                 .filter_map(|dependency| {
-                    if dependency.time_verified != self.current_epoch {
+                    if dependency.time_verified_or_calculated != self.current_epoch {
                         Some(dependency.node_to)
                     } else {
                         None
@@ -48,7 +50,8 @@ impl Database {
                 let child_node = self.nodes.remove(&child_node_id).expect("node to exist");
                 let new_value = (child_node.calculate)(self);
                 if child_node.value != new_value {
-                    has_changed = true
+                    has_changed = true;
+                    break;
                 }
                 self.nodes.insert(child_node_id, child_node);
             }
@@ -65,29 +68,64 @@ impl Database {
                 if *value != new_value {
                     node.value = Box::new(new_value.clone());
                 }
-                new_value
+                (new_value, 0)
             } else {
-                self.nodes
-                    .get(&node_id)
-                    .expect("node to exist")
-                    .value
-                    .as_any()
-                    .downcast_ref::<TOutput>()
-                    .expect("should be of expected type")
-                    .clone()
+                let derived_node = self.nodes.get(&node_id).expect("node to exist");
+                (
+                    derived_node
+                        .value
+                        .as_any()
+                        .downcast_ref::<TOutput>()
+                        .expect("should be of expected type")
+                        .clone(),
+                    derived_node.time_calculated,
+                )
             }
         } else {
+            self.dependency_stack.push(vec![]);
             let value = inner_fn(self, param.clone());
-            let _node = DerivedNode {
+            let dependencies_of_last_call_with_time_calculated = self
+                .dependency_stack
+                .pop()
+                .expect("dependency stack to not be empty");
+
+            let (dependencies_of_last_call, time_calculated) =
+                dependencies_of_last_call_with_time_calculated
+                    .into_iter()
+                    .fold(
+                        (vec![], 0),
+                        |(mut deps_so_far, mut max_so_far), (time_calculated, dep)| {
+                            deps_so_far.push(dep);
+                            max_so_far = std::cmp::max(max_so_far, time_calculated);
+                            (deps_so_far, max_so_far)
+                        },
+                    );
+
+            let node = DerivedNode {
                 value: Box::new(value.clone()),
                 time_verified: self.current_epoch,
-                time_calculated: 0,
-                dependencies: vec![],
-                calculate: Box::new(|database| Box::new(inner_fn(database, param.clone()))),
+                time_calculated,
+                dependencies: dependencies_of_last_call,
+                calculate: Box::new(move |database| Box::new(inner_fn(database, param.clone()))),
             };
-            // TODO: insert a new node
-            value
-        }
+            self.nodes.insert(node_id, node);
+
+            (value, time_calculated)
+        };
+
+        let last_dependency_stack = self
+            .dependency_stack
+            .last_mut()
+            .expect("expected dependency stack to not be empty");
+        last_dependency_stack.push((
+            time_calculated,
+            Dependency {
+                node_to: node_id,
+                time_verified_or_calculated: self.current_epoch,
+            },
+        ));
+
+        value
     }
 }
 
@@ -107,7 +145,7 @@ impl NodeId {
 #[derive(Clone, Copy)]
 pub struct Dependency {
     node_to: NodeId,
-    time_verified: usize,
+    time_verified_or_calculated: usize,
 }
 
 pub struct DerivedNode {
--- crates/compiler-playground/src/main.rs
+++ crates/compiler-playground/src/main.rs
@@ -10,23 +10,34 @@ fn main() {
     let mut database = Database {
         current_epoch: 0,
         nodes: HashMap::new(),
+        dependency_stack: vec![],
     };
 
     let path = PathBuf::from("demos/github-demo/schema.graphql");
 
+    database.set("file_text", "foo.txt", Box::new("hello"));
+
     let _ = database.calculate("read_and_parse_schema", path, |db, param| {
-        let schema = db.calculate("read_schema", param, |_, param2| {
-            let content = read_schema_file(&param2).unwrap();
-            let schema_text_source = TextSource {
-                path: param2
-                    .to_str()
-                    .expect("Expected schema to be valid string")
-                    .intern()
-                    .into(),
-                span: None,
-            };
-            parse_schema(&content, schema_text_source).unwrap()
-        });
-        schema
+        database.get("file_text", "foo.txt");
+        database.get_or_set_default("file_text", "foo.txt");
+        database.get_or_set("file_text", "foo.txt", Box::new("ASFASDF"));
+
+        read_parsed_derived(db, param)
     });
+}
+
+fn read_parsed_derived(db: &mut Database, param: PathBuf) -> _ {
+    let schema = db.calculate("read_schema", param, |_, param2| {
+        let content = read_schema_file(&param2).unwrap();
+        let schema_text_source = TextSource {
+            path: param2
+                .to_str()
+                .expect("Expected schema to be valid string")
+                .intern()
+                .into(),
+            span: None,
+        };
+        parse_schema(&content, schema_text_source).unwrap()
+    });
+    schema
 }
