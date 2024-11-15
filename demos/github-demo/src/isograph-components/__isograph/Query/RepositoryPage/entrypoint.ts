8c61c095a474436a48ecd9f653c9bc844d31ba44
Robert Balicki <robert.balicki@gmail.com>
2024-11-15 00:12:14 -0500

    oops - things were build using a compiler from the wrong commit

--- demos/github-demo/src/isograph-components/__isograph/Query/RepositoryPage/entrypoint.ts
+++ demos/github-demo/src/isograph-components/__isograph/Query/RepositoryPage/entrypoint.ts
@@ -316,6 +316,7 @@ const artifact: IsographEntrypoint<
     queryText,
     normalizationAst,
   },
+  concreteType: "Query",
   readerWithRefetchQueries: {
     kind: "ReaderWithRefetchQueries",
     nestedRefetchQueries,
