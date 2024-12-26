1e6efe4174abc920497c3916eb1864cd3ea57372
Patryk Wałach <35966385+PatrykWalach@users.noreply.github.com>
2024-12-26 10:42:37 +0100

    Fix `isograph-config-schema` being different in ci (#294)
    - write the JSON schema file directly from rust. Turns out using `stdout > file` results in different behavior in Windows and Linux, which this should fix

--- crates/isograph_config/src/main.rs
+++ crates/isograph_config/src/main.rs
@@ -1,7 +1,14 @@
+use std::fs;
+
 use isograph_config::IsographProjectConfig;
 use schemars::schema_for;
 
 fn main() {
     let schema = schema_for!(IsographProjectConfig);
-    println!("{}", serde_json::to_string_pretty(&schema).unwrap());
+
+    fs::write(
+        "./libs/isograph-compiler/isograph-config-schema.json",
+        serde_json::to_string_pretty(&schema).unwrap(),
+    )
+    .unwrap();
 }
