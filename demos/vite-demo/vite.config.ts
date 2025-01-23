421d6fafc14559dff492650f49167c8fee665129
Iha Shin (신의하) <me@xiniha.dev>
2025-01-24 03:00:17 +0900

    Support emitting ESM imports from Babel transform (#324)
    By default, babel will use esmodule imports for entrypoint literals

--- demos/vite-demo/vite.config.ts
+++ demos/vite-demo/vite.config.ts
@@ -1,7 +1,6 @@
 import path from 'path';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import commonjs from 'vite-plugin-commonjs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,11 +8,6 @@ export default defineConfig({
     react({
       babel: {
         babelrc: true,
-      },
-    }),
-    commonjs({
-      advanced: {
-        importRules: 'merge',
       },
     }),
   ],
