a1b66e5d4e1bfca6a67225406c59fc096b46a022
Patryk Wałach <35966385+PatrykWalach@users.noreply.github.com>
2025-02-01 19:08:58 +0100

    parse client pointer `to` keyword (#356)

--- crates/isograph_lang_parser/fixtures/pointer-directives.input.js
+++ crates/isograph_lang_parser/fixtures/pointer-directives.input.js
@@ -1,6 +1,6 @@
 // Directives are disallowed when we process the pointer, but allowed at parse time
 export const pointer = iso(`
-  pointer User.bestFriend @directives @allowed @at @parse @time {
+  pointer User.bestFriend to User @directives @allowed @at @parse @time {
     friends {
       id
       closeness
