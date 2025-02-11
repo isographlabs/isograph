85625ee04561428e487b111e77a09263a2267dd8
PatrykWalach <35966385+PatrykWalach@users.noreply.github.com>
2025-02-11 22:04:23 +0100

    add demo

--- demos/pet-demo/src/components/HomeRoute.tsx
+++ demos/pet-demo/src/components/HomeRoute.tsx
@@ -6,8 +6,8 @@ import { ErrorBoundary } from './ErrorBoundary';
 import { FullPageLoading } from './routes';
 
 export const HomeRoute = iso(`
-  field Query.HomeRoute @component {
-    pets {
+  field Query.HomeRoute($search: String, $statsWhere: PetStatsWhere) @component {
+    pets(where: { name: { like: $search }, stats: $statsWhere }) {
       id
       PetSummaryCard
     }
@@ -28,7 +28,16 @@ export const HomeRoute = iso(`
 export function HomeRouteLoader() {
   const { fragmentReference } = useLazyReference(
     iso(`entrypoint Query.HomeRoute`),
-    {},
+    {
+      search: '',
+      statsWhere: {
+        weight: {
+          gt: 2,
+          eq: null,
+          lt: null,
+        },
+      },
+    },
   );
 
   return (
