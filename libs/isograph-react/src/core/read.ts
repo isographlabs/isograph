c7b02bba40aed80cb9a52e90712cede5e6929432
PatrykWalach <35966385+PatrykWalach@users.noreply.github.com>
2025-02-22 22:02:49 +0100

    split readData

--- libs/isograph-react/src/core/read.ts
+++ libs/isograph-react/src/core/read.ts
@@ -194,6 +194,7 @@ function readData<TReadFromStore>(
           variables,
           nestedRefetchQueries,
           networkRequest,
+          networkRequestOptions,
           (ast, root) =>
             readData(
               environment,
@@ -643,6 +644,7 @@ export function readLinkedFieldData(
   variables: Variables,
   nestedRefetchQueries: RefetchQueryNormalizationArtifactWrapper[],
   networkRequest: PromiseWrapper<void, any>,
+  networkRequestOptions: NetworkRequestReaderOptions,
   readData: <TReadFromStore>(
     ast: ReaderAst<TReadFromStore>,
     root: Link,
@@ -739,6 +741,7 @@ export function readLinkedFieldData(
               environment,
               fragment,
               readerWithRefetchQueries.readerArtifact.fieldName,
+              networkRequestOptions,
             ),
           }
         : undefined),
