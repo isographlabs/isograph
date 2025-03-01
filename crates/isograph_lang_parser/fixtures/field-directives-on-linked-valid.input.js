be7e95da6b26fad6bbf8fc4eaf8b7fbffe7f3309
Robert Balicki <robert.balicki@gmail.com>
2025-02-28 23:48:08 -0500

    remove now-invalid linked field fixtures (with @loadable)

--- crates/isograph_lang_parser/fixtures/field-directives-on-linked-valid.input.js
+++ crates/isograph_lang_parser/fixtures/field-directives-on-linked-valid.input.js
@@ -5,20 +5,6 @@ export const BasicField = iso(`
   }
 `)();
 
-export const BasicField2 = iso(`
-  field Type.Name {
-    linked @loadable(lazyLoadArtifact: true) {
-    }
-  }
-`)();
-
-export const BasicField3 = iso(`
-  field Type.Name {
-    linked @loadable(lazyLoadArtifact: false) {
-    }
-  }
-`)();
-
 export const updatable = iso(`
   field Type.Name {
     linked @updatable {
