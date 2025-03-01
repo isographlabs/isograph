b52ad6a43dbcabcd411c3df4d28b71fc16c9ebef
Robert Balicki <robert.balicki@gmail.com>
2025-02-28 23:36:45 -0500

    rename files

--- crates/isograph_lang_types/src/lib.rs
+++ crates/isograph_lang_types/src/lib.rs
@@ -1,13 +1,13 @@
 mod client_field_declaration;
-mod directives;
 mod entrypoint_declaration;
 mod id_types;
+mod isograph_directives;
 mod isograph_type_annotation;
 mod scalar_field_valid_directive_set;
 
 pub use client_field_declaration::*;
-pub use directives::*;
 pub use entrypoint_declaration::*;
 pub use id_types::*;
+pub use isograph_directives::*;
 pub use isograph_type_annotation::*;
 pub use scalar_field_valid_directive_set::*;
