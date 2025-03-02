293e30f4721e6503c6e3aa48b5b8962578768616
Edwin Santos <edwin.santos.kov@gmail.com>
2025-03-02 16:33:39 -0500

    Prelim work on client field directives

--- crates/isograph_lang_types/src/lib.rs
+++ crates/isograph_lang_types/src/lib.rs
@@ -9,9 +9,9 @@ mod selection_directive_set;
 
 pub use base_types::*;
 pub use client_field_declaration::*;
+pub use client_field_directive_set::*;
 pub use entrypoint_declaration::*;
 pub use id_types::*;
 pub use isograph_directives::*;
 pub use isograph_type_annotation::*;
 pub use selection_directive_set::*;
-pub use client_field_directive_set::*;
