ed5c5147bea967a975d1268f3b0add30a58adfa7
Robert Balicki <robert.balicki@gmail.com>
2025-03-01 19:03:07 -0500

    move stuff into its own file

---
+++ crates/isograph_schema/src/object_type_definition.rs
@@ -0,0 +1,63 @@
+use common_lang_types::{
+    DescriptionValue, GraphQLInterfaceTypeName, IsographObjectTypeName, WithLocation, WithSpan,
+};
+use graphql_lang_types::{
+    GraphQLConstantValue, GraphQLDirective, GraphQLFieldDefinition,
+    GraphQLInputObjectTypeDefinition, GraphQLInterfaceTypeDefinition, GraphQLObjectTypeDefinition,
+};
+
+#[derive(Clone, Eq, PartialEq, Ord, PartialOrd, Debug)]
+pub struct IsographObjectTypeDefinition {
+    pub description: Option<WithSpan<DescriptionValue>>,
+    pub name: WithLocation<IsographObjectTypeName>,
+    // maybe this should be Vec<WithSpan<IsographObjectTypeName>>>
+    pub interfaces: Vec<WithLocation<GraphQLInterfaceTypeName>>,
+    /// Directives that we don't know about. Maybe this should be validated to be
+    /// empty, or not exist.
+    pub directives: Vec<GraphQLDirective<GraphQLConstantValue>>,
+    // TODO the spans of these fields are wrong
+    // TODO use a shared field type
+    pub fields: Vec<WithLocation<GraphQLFieldDefinition>>,
+}
+
+impl From<GraphQLObjectTypeDefinition> for IsographObjectTypeDefinition {
+    fn from(object_type_definition: GraphQLObjectTypeDefinition) -> Self {
+        IsographObjectTypeDefinition {
+            description: object_type_definition.description,
+            name: object_type_definition.name.map(|x| x.into()),
+            interfaces: object_type_definition.interfaces,
+            directives: object_type_definition.directives,
+            fields: object_type_definition.fields,
+        }
+    }
+}
+
+impl From<GraphQLInterfaceTypeDefinition> for IsographObjectTypeDefinition {
+    fn from(value: GraphQLInterfaceTypeDefinition) -> Self {
+        Self {
+            description: value.description,
+            name: value.name.map(|x| x.into()),
+            interfaces: value.interfaces,
+            directives: value.directives,
+            fields: value.fields,
+        }
+    }
+}
+
+// TODO this is bad. We should instead convert both GraphQL types to a common
+// Isograph type
+impl From<GraphQLInputObjectTypeDefinition> for IsographObjectTypeDefinition {
+    fn from(value: GraphQLInputObjectTypeDefinition) -> Self {
+        Self {
+            description: value.description,
+            name: value.name.map(|x| x.into()),
+            interfaces: vec![],
+            directives: value.directives,
+            fields: value
+                .fields
+                .into_iter()
+                .map(|with_location| with_location.map(From::from))
+                .collect(),
+        }
+    }
+}
