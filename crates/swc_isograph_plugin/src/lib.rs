f93bd9ef4d2056eb4d0715dc70bdc1a6a749212a
Pablo Crovetto <jpcc077@gmail.com>
2025-03-15 20:44:04 -0500

    Update prepack command on isograph-swc-plugin

--- crates/swc_isograph_plugin/src/lib.rs
+++ crates/swc_isograph_plugin/src/lib.rs
@@ -1,7 +1,6 @@
 use common_lang_types::CurrentWorkingDirectory;
 use intern::string_key::Intern;
 use isograph_config;
-use serde_json::Value;
 use std::path::PathBuf;
 use swc_common::{plugin::metadata::TransformPluginMetadataContextKind, FileName};
 use swc_core::{
@@ -35,28 +34,6 @@ fn isograph_plugin_transform(
     } else {
         FileName::Anon
     };
-
-    // let plugin_config: Value = serde_json::from_str(
-    //     &metadata
-    //         .get_transform_plugin_config()
-    //         .expect("failed to get plugin config for isograph"),
-    // )
-    // .expect("Should provide plugin config");
-
-    // let artifact_directory = plugin_config["artifact_directory"]
-    //     .as_str()
-    //     .map(PathBuf::from);
-    // let project_root = plugin_config["project_root"].as_str().map(PathBuf::from);
-
-    // let language = plugin_config["language"]
-    //     .as_str()
-    //     .map_or(RelayLanguageConfig::TypeScript, |v| v.try_into().unwrap());
-    // let output_file_extension = plugin_config["outputFileExtension"]
-    //     .as_str()
-    //     .map_or(OutputFileExtension::Undefined, |v| v.try_into().unwrap());
-    // let eager_es_modules = plugin_config["eagerEsModules"]
-    //     .as_bool()
-    //     .unwrap_or_default();
 
     let mut isograph = swc_isograph::isograph(&config, filename, Some(metadata.unresolved_mark));
 
