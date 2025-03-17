1b93ed2af07534fed6ff88aad772d0d126b72dbe
PatrykWalach <35966385+PatrykWalach@users.noreply.github.com>
2025-03-17 22:09:38 +0100

    generate client pointers `output_type.ts`

--- crates/isograph_schema/src/accessible_client_fields_iterator.rs
+++ crates/isograph_schema/src/accessible_client_fields_iterator.rs
@@ -1,16 +1,14 @@
-use common_lang_types::WithSpan;
-use isograph_lang_types::{DefinitionLocation, SelectionTypeContainingSelections};
-
 use crate::{
-    ClientFieldOrPointer, OutputFormat, ValidatedClientField, ValidatedSchema, ValidatedSelection,
-    ValidatedSelectionType,
+    ClientFieldOrPointer, OutputFormat, ValidatedSchema, ValidatedSelection, ValidatedSelectionType,
 };
+use common_lang_types::WithSpan;
+use isograph_lang_types::{DefinitionLocation, SelectionType, SelectionTypeContainingSelections};
 
 // This should really be replaced with a proper visitor, or something
 pub fn accessible_client_fields<'a, TOutputFormat: OutputFormat>(
     selection_type: &'a ValidatedSelectionType<'a, TOutputFormat>,
     schema: &'a ValidatedSchema<TOutputFormat>,
-) -> impl Iterator<Item = &'a ValidatedClientField<TOutputFormat>> + 'a {
+) -> impl Iterator<Item = ValidatedSelectionType<'a, TOutputFormat>> + 'a {
     AccessibleClientFieldIterator {
         selection_set: selection_type.selection_set_for_parent_query(),
         index: 0,
@@ -29,7 +27,7 @@ struct AccessibleClientFieldIterator<'a, TOutputFormat: OutputFormat> {
 impl<'a, TOutputFormat: OutputFormat> Iterator
     for AccessibleClientFieldIterator<'a, TOutputFormat>
 {
-    type Item = &'a ValidatedClientField<TOutputFormat>;
+    type Item = ValidatedSelectionType<'a, TOutputFormat>;
 
     fn next(&mut self) -> Option<Self::Item> {
         if let Some(iterator) = &mut self.sub_iterator {
@@ -55,7 +53,7 @@ impl<'a, TOutputFormat: OutputFormat> Iterator
                             DefinitionLocation::Client(client_field_id) => {
                                 let nested_client_field = self.schema.client_field(client_field_id);
                                 self.index += 1;
-                                return Some(nested_client_field);
+                                return Some(SelectionType::Scalar(nested_client_field));
                             }
                         }
                     }
@@ -65,6 +63,17 @@ impl<'a, TOutputFormat: OutputFormat> Iterator
                             schema: self.schema,
                             index: 0,
                             sub_iterator: None,
+                        };
+
+                        match linked_field.associated_data.field_id {
+                            DefinitionLocation::Client(client_pointer_id) => {
+                                let nested_client_pointer =
+                                    self.schema.client_pointer(client_pointer_id);
+                                self.sub_iterator = Some(Box::new(iterator));
+                                self.index += 1;
+                                return Some(SelectionType::Object(nested_client_pointer));
+                            }
+                            DefinitionLocation::Server(_) => {}
                         };
                         let next = iterator.next();
                         if next.is_some() {
