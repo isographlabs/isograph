c0aa6f4662b7a474def7713238d799161da60cc9
Robert Balicki <robert.balicki@gmail.com>
2025-04-17 08:46:58 +0800

    do things right

--- docs-website/src/pages/discord.tsx
+++ docs-website/src/pages/discord.tsx
@@ -1,5 +1,31 @@
-export default () => {
+import Layout from '@theme/Layout';
+import { useEffect } from 'react';
+
+export default function Home(): JSX.Element {
+  useEffect(() => {
+    // @ts-expect-error
+    window.location = 'https://discord.gg/qcHUxb6deQ';
+  }, []);
   return (
-    <script>window.location.href = 'https://discord.gg/qcHUxb6deQ';</script>
+    <Layout
+      title={`Isograph, the UI framework for teams that move fast without breaking things`}
+      description="Isograph, the UI framework for teams that move fast without breaking things"
+    >
+      <main>
+        <div
+          style={{
+            flexDirection: 'row',
+            display: 'flex',
+            justifyContent: 'center',
+          }}
+        >
+          <h1 style={{ paddingTop: 50 }}>
+            <a href="https://discord.gg/qcHUxb6deQ">
+              Click here to join the Isograph discord
+            </a>
+          </h1>
+        </div>
+      </main>
+    </Layout>
   );
-};
+}
