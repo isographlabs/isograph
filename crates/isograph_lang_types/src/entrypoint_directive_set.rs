ee54941f7a2fc2b5718748a197730e50d7b96bf1
Robert Balicki <robert.balicki@gmail.com>
2025-05-04 14:24:55 -0400

    rename directive parameters to lazyLoad for entrypoint declaration

--- crates/isograph_lang_types/src/entrypoint_directive_set.rs
+++ crates/isograph_lang_types/src/entrypoint_directive_set.rs
@@ -12,8 +12,9 @@ pub enum EntrypointDirectiveSet {
 #[derive(Deserialize, Debug, Default, Clone, PartialEq, PartialOrd, Ord, Eq, Copy, Hash)]
 #[serde(deny_unknown_fields, rename_all = "camelCase")]
 pub struct LazyLoadDirectiveSet {
-    pub component: ComponentDirectiveParameters,
+    pub lazy_load: LazyLoadDirectiveParameters,
 }
+
 #[derive(Deserialize, Debug, Default, Clone, Copy, PartialEq, PartialOrd, Ord, Eq, Hash)]
 #[serde(deny_unknown_fields, rename_all = "camelCase")]
-pub struct ComponentDirectiveParameters {}
+pub struct LazyLoadDirectiveParameters {}
