126076c7407a699ff174c40da8fc89ca2e2cd934
PatrykWalach <35966385+PatrykWalach@users.noreply.github.com>
2025-05-08 10:32:49 +0200

    fix generated ast dsa

--- crates/isograph_schema/src/isograph_schema.rs
+++ crates/isograph_schema/src/isograph_schema.rs
@@ -709,7 +709,7 @@ impl<TNetworkProtocol: NetworkProtocol> ServerEntityData<TNetworkProtocol> {
 #[derive(Debug, Clone, PartialEq, Eq, Hash, PartialOrd, Ord)]
 pub struct PathToRefetchField {
     pub linked_fields: Vec<NormalizationKey>,
-    pub field_name: ClientSelectableName,
+    pub field_name: SelectionType<ClientScalarSelectableName, NameAndArguments>,
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash, PartialOrd, Ord)]
