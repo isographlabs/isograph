53462644aeae6a56539c02e621a9e0ca3ac1af5e
Robert Balicki <robert.balicki@gmail.com>
2025-05-26 22:42:01 -0400

    oops - accidentally commented out line. fix that

--- crates/isograph_compiler/src/create_schema.rs
+++ crates/isograph_compiler/src/create_schema.rs
@@ -110,7 +110,7 @@ pub fn create_schema<TNetworkProtocol: NetworkProtocol>(
         process_iso_literals(&mut unvalidated_isograph_schema, contains_iso)?;
     unprocessed_items.extend(unprocessed_client_types);
 
-    // unvalidated_isograph_schema.add_link_fields()?;
+    unvalidated_isograph_schema.add_link_fields()?;
 
     unvalidated_isograph_schema.entrypoints = validate_entrypoints(
         &unvalidated_isograph_schema,
