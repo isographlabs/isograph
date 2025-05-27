869be713a355dc07c29dfd5a9dfe129609c35bde
Robert Balicki <robert.balicki@gmail.com>
2025-05-26 22:41:00 -0400

    fix tests

--- crates/swc_isograph_plugin/tests/fixtures/base/should_transform_iso_fn_to_a_import_call/output.js
+++ crates/swc_isograph_plugin/tests/fixtures/base/should_transform_iso_fn_to_a_import_call/output.js
@@ -1,5 +1,5 @@
-import _HomeRoute from "./__isograph/Query/HomeRoute/entrypoint.ts";
+import _Query__HomeRoute from "./__isograph/Query/HomeRoute/entrypoint.ts";
 
 function test() {
-  const a = _HomeRoute;
+  const a = _Query__HomeRoute;
 }
