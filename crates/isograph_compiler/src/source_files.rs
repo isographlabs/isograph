8b1e746a9b776272f0504ebc7abb19e7720f169c
Robert Balicki <robert.balicki@gmail.com>
2025-07-27 17:07:37 -0400

    unneeded public fn

--- crates/isograph_compiler/src/source_files.rs
+++ crates/isograph_compiler/src/source_files.rs
@@ -244,7 +244,7 @@ pub fn read_schema(
     Ok(schema_id)
 }
 
-pub fn read_schema_file(path: &PathBuf) -> Result<String, BatchCompileError> {
+fn read_schema_file(path: &PathBuf) -> Result<String, BatchCompileError> {
     let current_dir = std::env::current_dir().expect("current_dir should exist");
     let joined = current_dir.join(path);
     let canonicalized_existing_path =
