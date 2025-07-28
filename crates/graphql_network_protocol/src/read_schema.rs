829cc357980d1a62d5440cb2bc5e7f9d2cadcb31
Robert Balicki <robert.balicki@gmail.com>
2025-07-27 21:07:41 -0400

    get rid of unused error enum

--- crates/graphql_network_protocol/src/read_schema.rs
+++ crates/graphql_network_protocol/src/read_schema.rs
@@ -1,4 +1,4 @@
-use std::{collections::BTreeMap, path::PathBuf, str::Utf8Error};
+use std::collections::BTreeMap;
 
 use common_lang_types::{RelativePathToSourceFile, WithLocation};
 use graphql_lang_types::{GraphQLTypeSystemDocument, GraphQLTypeSystemExtensionDocument};
@@ -6,7 +6,6 @@ use graphql_schema_parser::{parse_schema, parse_schema_extensions, SchemaParseEr
 use isograph_lang_types::SchemaSource;
 use pico::{Database, MemoRef, SourceId};
 use pico_macros::memo;
-use thiserror::Error;
 
 #[allow(clippy::type_complexity)]
 #[memo]
@@ -19,7 +18,7 @@ pub fn parse_graphql_schema(
         MemoRef<GraphQLTypeSystemDocument>,
         BTreeMap<RelativePathToSourceFile, MemoRef<GraphQLTypeSystemExtensionDocument>>,
     ),
-    BatchCompileError,
+    WithLocation<SchemaParseError>,
 > {
     let SchemaSource {
         content,
@@ -40,40 +39,11 @@ pub fn parse_graphql_schema(
     Ok((db.intern(schema), schema_extensions))
 }
 
-#[derive(Error, Debug, Clone, PartialEq, Eq)]
-pub enum BatchCompileError {
-    #[error("Unable to load schema file at path {path:?}.\nReason: {message}")]
-    UnableToLoadSchema { path: PathBuf, message: String },
-
-    #[error("Attempted to load the graphql schema at the following path: {path:?}, but that is not a file.")]
-    SchemaNotAFile { path: PathBuf },
-
-    #[error("Unable to read the file at the following path: {path:?}.\nReason: {message}")]
-    UnableToReadFile { path: PathBuf, message: String },
-
-    #[error("Unable to create schema.\nReason: {0}")]
-    UnableToCreateSchema(#[from] WithLocation<isograph_schema::CreateAdditionalFieldsError>),
-
-    #[error("Error when processing an entrypoint declaration.\nReason: {0}")]
-    ErrorWhenProcessingEntrypointDeclaration(
-        #[from] WithLocation<isograph_schema::ValidateEntrypointDeclarationError>,
-    ),
-
-    #[error("Unable to strip prefix.\nReason: {0}")]
-    UnableToStripPrefix(#[from] std::path::StripPrefixError),
-
-    #[error("Unable to convert file {path:?} to utf8.\nDetailed reason: {reason}")]
-    UnableToConvertToString { path: PathBuf, reason: Utf8Error },
-
-    #[error("Unable to parse schema.\n\n{0}")]
-    UnableToParseSchema(#[from] WithLocation<SchemaParseError>),
-}
-
 #[memo]
 pub fn parse_schema_extensions_file(
     db: &Database,
     schema_extension_source_id: SourceId<SchemaSource>,
-) -> Result<MemoRef<GraphQLTypeSystemExtensionDocument>, BatchCompileError> {
+) -> Result<MemoRef<GraphQLTypeSystemExtensionDocument>, WithLocation<SchemaParseError>> {
     let SchemaSource {
         content,
         text_source,
