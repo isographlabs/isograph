7f8a6f708c9afcc88eb19e18dafc37ffe9325d59
Robert Balicki <robert.balicki@gmail.com>
2025-08-02 15:29:55 -0400

    add note about bug

--- crates/isograph_compiler/src/watch.rs
+++ crates/isograph_compiler/src/watch.rs
@@ -44,6 +44,7 @@ pub async fn handle_watch_command<TNetworkProtocol: NetworkProtocol + 'static>(
                     );
                     state = CompilerState::new(config_location.clone(), current_working_directory)?;
                     watcher.stop();
+                    // TODO is this a bug? Will we continue to watch the old folders? I think so.
                     (rx, watcher) = create_debounced_file_watcher(&config);
                     WithDuration::new(|| compile::<TNetworkProtocol>(&state.db))
                 } else {
