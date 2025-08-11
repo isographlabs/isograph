339ec9e48c975864d7f8c15716cb14ea4d142482
Robert Balicki <robert.balicki@gmail.com>
2025-08-10 22:58:25 -0400

    update comment

--- crates/resolve_position/src/lib.rs
+++ crates/resolve_position/src/lib.rs
@@ -20,7 +20,7 @@ use common_lang_types::Span;
 ///
 /// ```txt
 /// ResolvedNode::ScalarSelectableName(ScalarSelectableNamePath {
-///   inner: &WithSpan<ScalarSelectableName>,
+///   inner: &ScalarSelectableName,
 ///   parent: ClientScalarSelectionPath {
 ///     item: &ClientScalarSelection,
 ///     parent: ClientScalarSelectionParent::ClientFieldDefinition(
@@ -48,30 +48,22 @@ use common_lang_types::Span;
 ///
 /// ## Implementation details
 ///
-/// In order to implement `ResolvePosition`, we define the outermost enum, and
-/// for each AST node:
-/// - define a parent path enum, or alias the parent path as `[NODE_TYPE]Parent`.
+/// In order to implement `ResolvePosition`, we define the `ResolvedNode`` enum,
+/// and for each AST node:
+/// - define a parent path enum, or alias the parent path as `[NODE_TYPE]Parent`
+///   (if there is only one type of parent).
 /// - define a `[NODE_TYPE]Path` alias, containing `WithSpan<NODE_TYPE>` and the
 ///   parent type, and
 /// - implement the `ResolvePosition` trait for the AST node itself.
 ///
 /// The actual logic lives in the implementation of `ResolvePosition::resolve`
 /// for each AST node. The implementation must behave as follows:
-/// - It must call `.contains()` on each of its child nodes. When the child node
-///   containing the position is discovered, it must return the result of calling
-///   `.resolve()` on that child.
-/// - If no child's `.contains()` method returns true (i.e. no child contains the
-///   position), then the node must assume that it is the laf node and returns its
-///   own variant of `ResolvedNode`.
-///
-/// This approach is not ideal: there is an implicit (i.e. not enforced by the
-/// compiler) contract that each parent node must check the result of `.contains()`
-/// on a child before calling that child's `.resolve()` method. Ideally, `.resolve()`
-/// could return an `Option`. However, calling `.resolve()` moves the parent into
-/// the child's method, meaning that we cannot try out multiple potential children.
-/// Hence, the alternative of calling `.contains()` followed by `.resolve()`.
-///
-/// NOTE: I believe this can be improved by return a `ControlFlow` from `resolve()`.
+/// - It must check whether each subnode contains the position, most often by
+///   calling `field.span.contains(positio)`. When the child node containing the
+///   position is discovered, it must return the result of calling `.resolve()`
+///   on that child.
+/// - If no child contains the position, then the node must assume that it is
+///   the leaf node and returns its own variant of `ResolvedNode`.
 
 #[derive(Debug)]
 pub struct Path<Inner, Parent> {
