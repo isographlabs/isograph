499a2b7bda852abc518f7826819412f70893a365
PatrykWalach <35966385+PatrykWalach@users.noreply.github.com>
2025-08-27 23:47:54 +0200

    fix: file paths on Windows

--- crates/isograph_lsp/src/uri_file_path_ext.rs
+++ crates/isograph_lsp/src/uri_file_path_ext.rs
@@ -6,8 +6,14 @@ pub trait UriFilePathExt {
 
 impl UriFilePathExt for lsp_types::Uri {
     fn to_file_path(&self) -> Result<PathBuf, ()> {
-        url::Url::parse(self.as_str())
+        let mut file_path = url::Url::parse(self.as_str())
             .map_err(|_| ())?
-            .to_file_path()
+            .to_file_path();
+
+        if cfg!(target_os = "windows") {
+            file_path = std::fs::canonicalize(file_path?).map_err(|_| ());
+        }
+
+        file_path
     }
 }
