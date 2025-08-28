cdce7db9e0e7ebb76a8ec135075ceb56d4207cd5
Iha Shin <me@xiniha.dev>
2025-08-28 16:56:39 +0900

    parse `entrypoint @lazyLoad` params

--- crates/isograph_lang_types/src/entrypoint_directive_set.rs
+++ crates/isograph_lang_types/src/entrypoint_directive_set.rs
@@ -15,6 +15,48 @@ pub struct LazyLoadDirectiveSet {
     pub lazy_load: LazyLoadDirectiveParameters,
 }
 
-#[derive(Deserialize, Debug, Default, Clone, Copy, PartialEq, PartialOrd, Ord, Eq, Hash)]
+#[derive(Deserialize, Debug, Clone, Copy, PartialEq, PartialOrd, Ord, Eq, Hash)]
+#[serde(from = "LazyLoadDirectiveParametersInput")]
+pub struct LazyLoadDirectiveParameters {
+    #[serde(default)]
+    pub reader: bool,
+    #[serde(default)]
+    pub normalization: bool,
+}
+
+#[derive(Deserialize, Debug, Clone, Copy, PartialEq, PartialOrd, Ord, Eq, Hash)]
 #[serde(deny_unknown_fields, rename_all = "camelCase")]
-pub struct LazyLoadDirectiveParameters {}
+struct LazyLoadDirectiveParametersInput {
+    #[serde(default)]
+    reader: Option<bool>,
+    #[serde(default)]
+    normalization: Option<bool>,
+}
+
+impl From<LazyLoadDirectiveParametersInput> for LazyLoadDirectiveParameters {
+    fn from(value: LazyLoadDirectiveParametersInput) -> Self {
+        // @lazyLoad, @lazyLoad() => reader: true, normalization: true
+        // @lazyLoad(reader: true) => reader: true, normalization: false
+        // @lazyLoad(normalization: true) => reader: true, normalization: true
+        // otherwise user-written values used
+        match value {
+            LazyLoadDirectiveParametersInput {
+                reader: None,
+                normalization: None,
+            } => Self::default(),
+            _ => Self {
+                reader: value.reader.unwrap_or(true),
+                normalization: value.normalization.unwrap_or(false),
+            },
+        }
+    }
+}
+
+impl Default for LazyLoadDirectiveParameters {
+    fn default() -> Self {
+        Self {
+            reader: true,
+            normalization: true,
+        }
+    }
+}
