a0454bb8eb1041b2f937a9c9a6e992c32f053a0b
PatrykWalach <35966385+PatrykWalach@users.noreply.github.com>
2025-09-01 18:17:45 +0200

    delete SmartestPet

--- demos/pet-demo/src/components/__isograph/Query/SmartestPet/output_type.ts
+++
@@ -1,4 +0,0 @@
-import type { Link } from '@isograph/react';
-import type React from 'react';
-import { SmartestPet as resolver } from '../../../SmartestPet';
-export type Query__smartestPet__output_type = (Link | null);
