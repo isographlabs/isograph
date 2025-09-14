a6c1be5dfb5e1a57b76ea810849f0819792a501d
Robert Balicki <robert.balicki@gmail.com>
2025-09-14 20:37:38 +0200

    add comment

--- crates/graphql_network_protocol/src/process_type_system_definition.rs
+++ crates/graphql_network_protocol/src/process_type_system_definition.rs
@@ -439,6 +439,7 @@ fn process_scalar_definition(
             .description
             .map(|with_span| with_span.map(|dv| dv.into())),
         name: scalar_type_definition.name,
+        // TODO we should allow customization here
         javascript_name: *STRING_JAVASCRIPT_TYPE,
         network_protocol: std::marker::PhantomData,
     }
