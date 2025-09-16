e97b3a3a3bd8d0a26464836e0d6fc90f52bce725
PatrykWalach <35966385+PatrykWalach@users.noreply.github.com>
2025-09-16 15:49:19 +0200

    update some demos

---
+++ demos/pet-demo/src/components/__isograph/Pet/link/output_type.ts
@@ -0,0 +1,2 @@
+import type { Link } from '@isograph/react';
+export type Pet__link__output_type = Link<"Pet">;
--- demos/pet-demo/src/components/__isograph/Query/smartestPet/param_type.ts
+++ demos/pet-demo/src/components/__isograph/Query/smartestPet/param_type.ts
@@ -1,4 +1,4 @@
-import type { Link } from '@isograph/react';
+import { type Pet__link__output_type } from '../../Pet/link/output_type';
 
 export type Query__smartestPet__param = {
   readonly data: {
@@ -6,7 +6,7 @@ export type Query__smartestPet__param = {
       /**
 A store Link for the Pet type.
       */
-      readonly link: Link<"Pet">,
+      readonly link: Pet__link__output_type,
       readonly stats: ({
         readonly intelligence: (number | null),
       } | null),
