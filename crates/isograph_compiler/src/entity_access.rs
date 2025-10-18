d36e8e6f3839556e853758c2668c1426abd6afc9
Robert Balicki <robert.balicki@gmail.com>
2025-10-18 02:42:39 -0400

    Update server_object_entity to call TNetworkProtocol::parse_and_process_type_system_documents(db), instead of going through create_type_system_schema
    - this allows goto def to work even if there are duplicate types defined!!! (For other types)

--- crates/isograph_compiler/src/entity_access.rs
+++ crates/isograph_compiler/src/entity_access.rs
@@ -10,19 +10,26 @@ use crate::create_type_system_schema;
 pub fn server_object_entity<TNetworkProtocol: NetworkProtocol + 'static>(
     db: &IsographDatabase<TNetworkProtocol>,
     server_object_entity_name: ServerObjectEntityName,
+    // TODO this should return a Option<Vec<...>>, and the caller should enforce that it is length one.
+    // Goto def, for example, can support multiple!
 ) -> Option<ServerObjectEntity<TNetworkProtocol>> {
-    let memo_ref = create_type_system_schema(db);
-    let (schema, _expose_as_fields_to_insert, _fields_to_insert) = match memo_ref.deref() {
+    let memo_ref = TNetworkProtocol::parse_and_process_type_system_documents(db);
+    let (outcome, _) = match memo_ref.deref() {
         Ok(s) => s,
         Err(_) => return None,
     };
 
-    let entity = schema
-        .server_entity_data
-        .server_object_entity(server_object_entity_name);
+    let outcome = outcome.objects.get(&server_object_entity_name)?;
+
+    if let Some((entity, rest)) = outcome.split_first() {
+        if rest.is_empty() {
+            return Some(entity.0.server_object_entity.clone());
+        }
+    }
+
+    None
 
     // TODO return Option<&ServerObjectEntity> when this is supported
-    entity.cloned()
 }
 
 #[memo]
