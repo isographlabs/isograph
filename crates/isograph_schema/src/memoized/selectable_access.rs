d14b319722273440c2046c8e94dbc64f16519636
Robert Balicki <robert.balicki@gmail.com>
2025-11-02 17:25:16 -0600

    also return ServerSelectableName

--- crates/isograph_schema/src/memoized/selectable_access.rs
+++ crates/isograph_schema/src/memoized/selectable_access.rs
@@ -1,6 +1,6 @@
-use std::{collections::HashMap, ops::Deref};
+use std::ops::Deref;
 
-use common_lang_types::ServerObjectEntityName;
+use common_lang_types::{ServerObjectEntityName, ServerSelectableName};
 use pico_macros::legacy_memo;
 
 use crate::{
@@ -10,13 +10,17 @@ use crate::{
 
 /// A vector of all server selectables that are defined in the type system schema
 #[legacy_memo]
+#[expect(clippy::type_complexity)]
 pub fn server_selectables_vec<TNetworkProtocol: NetworkProtocol + 'static>(
     db: &IsographDatabase<TNetworkProtocol>,
     parent_server_object_entity_name: ServerObjectEntityName,
 ) -> Result<
     // TODO return the SelectableId with each Result, i.e. we should know
     // the parent type and selectable name infallibly
-    Vec<Result<OwnedServerSelectable<TNetworkProtocol>, FieldToInsertToServerSelectableError>>,
+    Vec<(
+        ServerSelectableName,
+        Result<OwnedServerSelectable<TNetworkProtocol>, FieldToInsertToServerSelectableError>,
+    )>,
     TNetworkProtocol::ParseTypeSystemDocumentsError,
 > {
     let memo_ref = TNetworkProtocol::parse_type_system_documents(db);
@@ -28,12 +32,15 @@ pub fn server_selectables_vec<TNetworkProtocol: NetworkProtocol + 'static>(
         .filter(|o| o.server_object_entity.item.name.item == parent_server_object_entity_name)
         .flat_map(|o| {
             o.fields_to_insert.iter().map(|field_to_insert| {
-                field_to_insert_to_server_selectable(
-                    db,
-                    parent_server_object_entity_name,
-                    field_to_insert,
+                (
+                    field_to_insert.item.name.item,
+                    field_to_insert_to_server_selectable(
+                        db,
+                        parent_server_object_entity_name,
+                        field_to_insert,
+                    )
+                    .map(|x| x.map_scalar(|(scalar, _)| scalar)),
                 )
-                .map(|x| x.map_scalar(|(scalar, _)| scalar))
             })
         })
         .collect())
