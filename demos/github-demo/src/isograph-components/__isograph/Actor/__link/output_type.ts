236abd008e7cc03a0c8305d161202b5161aca2ca
Robert Balicki <robert.balicki@gmail.com>
2025-11-12 00:33:54 -0500

    run compiler everywhere

---
+++ demos/github-demo/src/isograph-components/__isograph/Actor/__link/output_type.ts
@@ -0,0 +1,7 @@
+import type { Link } from '@isograph/react';
+export type Actor____link__output_type = 
+  | Link<"Bot">
+  | Link<"EnterpriseUserAccount">
+  | Link<"Mannequin">
+  | Link<"Organization">
+  | Link<"User">;
