20917e80a7086b8a73739880c011a9d07bbea511
Robert Balicki <robert.balicki@gmail.com>
2025-11-21 00:50:34 -0500

    add postfix dbg and disallow its use

--- crates/prelude/src/lib.rs
+++ crates/prelude/src/lib.rs
@@ -2,24 +2,33 @@ pub trait Postfix
 where
     Self: Sized,
 {
+    #[inline(always)]
     fn ok<E>(self) -> Result<Self, E> {
         Ok(self)
     }
 
+    #[inline(always)]
     fn err<T>(self) -> Result<T, Self> {
         Err(self)
     }
 
+    #[inline(always)]
     fn some(self) -> Option<Self> {
         Some(self)
     }
 
+    #[inline(always)]
     fn boxed(self) -> Box<Self> {
         Box::new(self)
+    }
+
+    #[inline(always)]
+    fn dbg(self) -> Self
+    where
+        Self: std::fmt::Debug,
+    {
+        dbg!(self)
     }
 }
 
 impl<T> Postfix for T {}
-
-// TODO postfix debug
-// TODO prevent that method ending up in main via clippy lint
