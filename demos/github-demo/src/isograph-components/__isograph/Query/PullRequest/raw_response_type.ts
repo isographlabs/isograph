74a0180b17b79f8839fbdb1d2b4828f46c3cb92f
Patryk Wałach <35966385+PatrykWalach@users.noreply.github.com>
2025-11-23 20:41:11 +0100

    mark nullable fields as optional in generated raw response types (#778)
    - If a network response can contain `null` for a value, make its type optional in the raw response type.
    - Isograph treats missing fields as null when normalizing.
    - In the future, we may want to make this configurable, but we can wait until customers ask for this!

--- demos/github-demo/src/isograph-components/__isograph/Query/PullRequest/raw_response_type.ts
+++ demos/github-demo/src/isograph-components/__isograph/Query/PullRequest/raw_response_type.ts
@@ -1,14 +1,14 @@
 export type Query__PullRequest__rawResponse = {
-  repository____owner___v_repositoryOwner____name___v_repositoryName: ({
+  repository____owner___v_repositoryOwner____name___v_repositoryName?: ({
     id: string,
-    pullRequest____number___v_pullRequestNumber: ({
+    pullRequest____number___v_pullRequestNumber?: ({
       id: string,
       bodyHTML: string,
       comments____last___l_10: {
-        edges: (ReadonlyArray<({
-          node: ({
+        edges?: (ReadonlyArray<({
+          node?: ({
             id: string,
-            author: ({
+            author?: ({
               __typename: string,
               login: string,
             } | null),
@@ -23,7 +23,7 @@ export type Query__PullRequest__rawResponse = {
   viewer: {
     id: string,
     avatarUrl: string,
-    name: (string | null),
+    name?: (string | null),
   },
 }
 
