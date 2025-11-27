76cdf58412591a4061104450f0dcc69755e7b131
Lucas Machado <lucasmachadorj@gmail.com>
2025-11-27 16:38:47 -0300

    Define ServerObjectEntityDirectives object. It's the root object diretive to be deserialized

--- crates/isograph_schema/src/create_additional_fields/mod.rs
+++ crates/isograph_schema/src/create_additional_fields/mod.rs
@@ -1,6 +1,8 @@
 mod argument_map;
 mod create_additional_fields_error;
+mod server_object_entity_directives;
 pub(crate) mod expose_field_directive;
 
 pub use create_additional_fields_error::*;
 pub use expose_field_directive::*;
+pub use server_object_entity_directives::*;
