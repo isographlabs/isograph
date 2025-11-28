d51875db26b278d669cfb87063d8458d014a6460
Lucas Machado <lucasmachadorj@gmail.com>
2025-11-28 03:18:48 -0300

    Add optional canonical id to the ServerObjectEntityDirectives
    We also enforces that the user will not declare any invalid directive
    name throught deny_unknown_fields

--- crates/isograph_schema/src/create_additional_fields/server_object_entity_directives.rs
+++ crates/isograph_schema/src/create_additional_fields/server_object_entity_directives.rs
@@ -1,9 +1,12 @@
+use crate::create_additional_fields::canonical_id_directive::CanonicalIdDirective;
 use crate::create_additional_fields::expose_field_directive::ExposeFieldDirective;
 use serde::Deserialize;
 
 #[derive(Deserialize, Debug, Clone, Default)]
-#[serde(rename_all = "camelCase")]
+#[serde(deny_unknown_fields, rename_all = "camelCase")]
 pub struct ServerObjectEntityDirectives {
     #[serde(default)]
     pub expose_field: Vec<ExposeFieldDirective>,
+    #[serde(default)]
+    pub canonical_id: Option<CanonicalIdDirective>,
 }
