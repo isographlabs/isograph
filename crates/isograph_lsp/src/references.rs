accce6da9d267e7b1ed25c375f3afa818d25c8e9
Robert Balicki <robert.balicki@gmail.com>
2025-11-30 13:24:57 -0500

    wip

--- crates/isograph_lsp/src/references.rs
+++ crates/isograph_lsp/src/references.rs
@@ -1,7 +1,14 @@
-use common_lang_types::{Span, relative_path_from_absolute_and_working_directory};
+use common_lang_types::{
+    ClientScalarSelectableName, Span, relative_path_from_absolute_and_working_directory,
+};
 use isograph_compiler::CompilerState;
-use isograph_lang_types::{ClientScalarSelectableNameWrapperParent, IsographResolvedNode};
-use isograph_schema::{IsographDatabase, NetworkProtocol, process_iso_literal_extraction};
+use isograph_lang_types::{
+    ClientScalarSelectableNameWrapperParent, IsographResolvedNode, SelectionType,
+};
+use isograph_schema::{
+    IsographDatabase, NetworkProtocol, accessible_client_selectables, client_selectable_map,
+    process_iso_literal_extraction, read_iso_literals_source_from_relative_path,
+};
 use lsp_types::{
     Position, Uri,
     request::{References, Request},
@@ -12,7 +19,9 @@ use resolve_position::ResolvePosition;
 
 use crate::{
     hover::get_iso_literal_extraction_from_text_position_params,
-    lsp_runtime_error::LSPRuntimeResult, uri_file_path_ext::UriFilePathExt,
+    location_utils::isograph_location_to_lsp_location,
+    lsp_runtime_error::{LSPRuntimeError, LSPRuntimeResult},
+    uri_file_path_ext::UriFilePathExt,
 };
 
 pub fn on_references<TNetworkProtocol: NetworkProtocol>(
@@ -65,17 +74,68 @@ fn on_references_impl<TNetworkProtocol: NetworkProtocol>(
             IsographResolvedNode::ClientScalarSelectableNameWrapper(
                 scalar_selectable_name_path,
             ) => {
-                let parent_object_entity_name = match scalar_selectable_name_path.parent {
+                let target_parent_object_entity_name = match scalar_selectable_name_path.parent {
                     ClientScalarSelectableNameWrapperParent::EntrypointDeclaration(
                         entrypoint_declaration_path,
                     ) => entrypoint_declaration_path.inner.parent_type.item,
                     ClientScalarSelectableNameWrapperParent::ClientFieldDeclaration(
                         client_field_declaration_path,
                     ) => client_field_declaration_path.inner.parent_type.item,
-                };
-                let scalar_selectable_name = *scalar_selectable_name_path.inner;
+                }
+                .dbg();
+                let target_scalar_selectable_name = *scalar_selectable_name_path.inner.dbg();
 
-                None
+                let map = client_selectable_map(db)
+                    .as_ref()
+                    .map_err(|_| LSPRuntimeError::ExpectedError)?;
+
+                map.iter()
+                    .flat_map(|(key, value)| {
+                        let selection_type = match value {
+                            Ok(val) => val,
+                            Err(_) => return vec![],
+                        };
+
+                        eprintln!("about to search selectables in {:?}", key);
+
+                        accessible_client_selectables(db, selection_type)
+                            .filter_map(|value| {
+                                match value.item {
+                                    SelectionType::Scalar(scalar_selection) => {
+                                        if scalar_selection.0 == target_parent_object_entity_name.0
+                                            && scalar_selection.1 == target_scalar_selectable_name.0
+                                        {
+                                            return Some(value.location.as_embedded_location()?);
+                                        }
+                                    }
+                                    SelectionType::Object(object_selection) => {
+                                        let selection_name: ClientScalarSelectableName =
+                                            object_selection.1.unchecked_conversion();
+                                        if object_selection.0 == target_parent_object_entity_name.0
+                                            && selection_name == target_scalar_selectable_name.0
+                                        {
+                                            return Some(value.location.as_embedded_location()?);
+                                        }
+                                    }
+                                };
+                                None
+                            })
+                            .flat_map(|location| {
+                                let content = match read_iso_literals_source_from_relative_path(
+                                    db,
+                                    location.text_source.relative_path_to_source_file,
+                                ) {
+                                    Some(s) => &s.content,
+                                    // Is this the correct behavior?
+                                    None => return None,
+                                };
+
+                                isograph_location_to_lsp_location(db, location, content)
+                            })
+                            .collect()
+                    })
+                    .collect::<Vec<_>>()
+                    .wrap_some()
             }
             IsographResolvedNode::ClientObjectSelectableNameWrapper(_) => None,
             IsographResolvedNode::SelectionSet(_) => None,
