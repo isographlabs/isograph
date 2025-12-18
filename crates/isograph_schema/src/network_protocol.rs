b14b23940122db9255b809702227df37e9e0a23d
Robert Balicki <robert.balicki@gmail.com>
2025-12-18 01:42:57 -0500

    remove outdated structs

--- crates/isograph_schema/src/network_protocol.rs
+++ crates/isograph_schema/src/network_protocol.rs
@@ -1,16 +1,13 @@
 use std::{collections::BTreeMap, fmt::Debug, hash::Hash};
 
 use common_lang_types::{
-    DiagnosticResult, EntityName, JavascriptName, QueryExtraInfo, QueryOperationName, QueryText,
-    SelectableName, WithLocation, WithNonFatalDiagnostics, WithSpan,
+    DiagnosticResult, EntityName, QueryExtraInfo, QueryOperationName, QueryText, SelectableName,
+    WithLocation, WithNonFatalDiagnostics,
 };
-use graphql_lang_types::{GraphQLInputValueDefinition, GraphQLTypeAnnotation};
-use isograph_lang_types::Description;
 
 use crate::{
-    ExposeFieldDirective, MemoRefSelectable, MemoRefServerEntity, MergedSelectionMap,
-    RefetchStrategy, RootOperationName, ServerObjectEntity, ValidatedVariableDefinition,
-    isograph_database::IsographDatabase,
+    MemoRefSelectable, MemoRefServerEntity, MergedSelectionMap, RefetchStrategy, RootOperationName,
+    ValidatedVariableDefinition, isograph_database::IsographDatabase,
 };
 
 #[derive(Clone, Eq, PartialEq, Hash, Debug, Default)]
@@ -59,47 +56,6 @@ pub trait NetworkProtocol:
         operation_name: EntityName,
         indentation_level: u8,
     ) -> QueryExtraInfo;
-}
-
-#[derive(Debug, Eq, PartialEq, Clone)]
-pub struct ProcessObjectTypeDefinitionOutcome<TNetworkProtocol: NetworkProtocol> {
-    pub server_object_entity: WithLocation<ServerObjectEntity<TNetworkProtocol>>,
-    pub fields_to_insert: Vec<WithLocation<FieldToInsert>>,
-    // TODO this seems sketch
-    pub expose_fields_to_insert: Vec<ExposeFieldToInsert>,
-}
-
-#[derive(Debug, Eq, PartialEq, Hash, Clone)]
-pub struct FieldToInsert {
-    pub description: Option<WithSpan<Description>>,
-    pub name: WithLocation<SelectableName>,
-    pub graphql_type: GraphQLTypeAnnotation<EntityName>,
-    /// An override type for the typename field. Normally, the JavaScript type is
-    /// acquired by going through graphql_type.inner(), but there is no separate
-    /// 'UserTypename' type in GraphQL. So we do this instead. This is horrible
-    /// data modeling, and should be fixed.
-    pub javascript_type_override: Option<JavascriptName>,
-    pub arguments: Vec<WithLocation<GraphQLInputValueDefinition>>,
-
-    // TODO we can probably restructure things to make this less awkward.
-    // As in, we should not return GraphQLFieldDefinitions to the isograph side,
-    // which is GraphQL-agnostic, and instead pass field definitions. These field
-    // definitions should have an associated_data: TNetworkProtocol::FieldAssociatedData
-    // or the like, which should carry this info.
-    //
-    // Then, that should be consumed by NetworkProtocol::generate_query_text, and also
-    // somehow by generate_merged_selection_set. (Is a merged selection set something
-    // that the network protocol should care about?? I don't think so, but how else
-    // do we add the __typename and link selections?)
-    pub is_inline_fragment: bool,
-}
-
-#[derive(Debug, Eq, PartialEq, Hash, Clone)]
-pub struct ExposeFieldToInsert {
-    pub expose_field_directive: ExposeFieldDirective,
-    // e.g. Query or Mutation
-    pub parent_object_name: EntityName,
-    pub description: Option<Description>,
 }
 
 #[derive(Debug, Clone, Copy)]
