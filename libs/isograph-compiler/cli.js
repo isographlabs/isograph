86e6361567c2831d25a421f4284a8a67e71e6cdf
Robert Balicki <robert.balicki@gmail.com>
2025-12-18 11:43:30 -0800

    chmod +x the binary in cli.js

--- libs/isograph-compiler/cli.js
+++ libs/isograph-compiler/cli.js
@@ -7,6 +7,13 @@ var spawn = require('child_process').spawn;
 var input = process.argv.slice(2);
 
 if (bin != null) {
+  try {
+    // Force 755 (rwxr-xr-x)
+    //
+    // If Isograph is installed with Bun, the binary isn't always executable.
+    chmodSync(bin, 0o755);
+  } catch {}
+
   spawn(bin, input, { stdio: 'inherit' }).on('exit', process.exit);
 } else {
   throw new Error(
