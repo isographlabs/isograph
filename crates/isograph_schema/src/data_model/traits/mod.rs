62ab3e44190aabc7c3fb11a6e19e98e0470a8ba2
Robert Balicki <robert.balicki@gmail.com>
2025-12-19 16:21:05 -0800

    more moving stuff around

--- crates/isograph_schema/src/data_model/traits/mod.rs
+++ crates/isograph_schema/src/data_model/traits/mod.rs
@@ -1,9 +1,7 @@
 mod client_or_server_scalar_selectable;
-mod object_selectables_trait;
 mod scalar_or_object_entity;
 mod selectable;
 
 pub use client_or_server_scalar_selectable::*;
-pub use object_selectables_trait::*;
 pub use scalar_or_object_entity::*;
 pub use selectable::*;
--- crates/isograph_schema/src/data_model/traits/selectable.rs
+++ crates/isograph_schema/src/data_model/traits/selectable.rs
@@ -1,8 +1,22 @@
 use isograph_lang_types::DefinitionLocation;
+use pico::MemoRef;
 
-use crate::{MemoRefClientSelectable, MemoRefServerSelectable};
+use crate::{
+    ClientObjectSelectable, MemoRefClientSelectable, MemoRefServerSelectable,
+    ServerObjectSelectable,
+};
 
 pub type MemoRefSelectable<TNetworkProtocol> = DefinitionLocation<
     MemoRefServerSelectable<TNetworkProtocol>,
     MemoRefClientSelectable<TNetworkProtocol>,
+>;
+
+pub type BorrowedObjectSelectable<'a, TNetworkProtocol> = DefinitionLocation<
+    &'a ServerObjectSelectable<TNetworkProtocol>,
+    &'a ClientObjectSelectable<TNetworkProtocol>,
+>;
+
+pub type MemoRefObjectSelectable<TNetworkProtocol> = DefinitionLocation<
+    MemoRef<ServerObjectSelectable<TNetworkProtocol>>,
+    MemoRef<ClientObjectSelectable<TNetworkProtocol>>,
 >;
