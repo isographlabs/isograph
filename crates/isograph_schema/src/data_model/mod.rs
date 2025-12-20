f29048c41dbd45a1b2e3b403cb1832a1f80b1198
Robert Balicki <robert.balicki@gmail.com>
2025-12-19 16:27:36 -0800

    more moving around

--- crates/isograph_schema/src/data_model/mod.rs
+++ crates/isograph_schema/src/data_model/mod.rs
@@ -1,9 +1,7 @@
 mod client_selectables;
 mod entities;
 mod server_selectables;
-mod traits;
 
 pub use client_selectables::*;
 pub use entities::*;
 pub use server_selectables::*;
-pub use traits::*;
--- crates/isograph_schema/src/data_model/traits/mod.rs
+++
@@ -1,3 +0,0 @@
-mod scalar_or_object_entity;
-
-pub use scalar_or_object_entity::*;
