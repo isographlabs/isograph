5eeff51dcffa3569ad002246d736dca3d99959f7
Robert Balicki <robert.balicki@gmail.com>
2025-12-19 16:16:28 -0800

    remove unused trait

--- crates/isograph_schema/src/data_model/traits/mod.rs
+++ crates/isograph_schema/src/data_model/traits/mod.rs
@@ -3,11 +3,9 @@ mod client_selectables_trait;
 mod object_selectables_trait;
 mod scalar_or_object_entity;
 mod selectable;
-mod server_selectables_trait;
 
 pub use client_or_server_scalar_selectable::*;
 pub use client_selectables_trait::*;
 pub use object_selectables_trait::*;
 pub use scalar_or_object_entity::*;
 pub use selectable::*;
-pub use server_selectables_trait::*;
