f598d4ccd88aa60c74165ee4ba7444b84a0b88ab
Robert Balicki <robert.balicki@gmail.com>
2025-12-19 16:20:06 -0800

    move file

--- crates/isograph_schema/src/data_model/traits/mod.rs
+++ crates/isograph_schema/src/data_model/traits/mod.rs
@@ -1,11 +1,9 @@
 mod client_or_server_scalar_selectable;
-mod client_selectables_trait;
 mod object_selectables_trait;
 mod scalar_or_object_entity;
 mod selectable;
 
 pub use client_or_server_scalar_selectable::*;
-pub use client_selectables_trait::*;
 pub use object_selectables_trait::*;
 pub use scalar_or_object_entity::*;
 pub use selectable::*;
