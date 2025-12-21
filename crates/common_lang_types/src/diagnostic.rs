e96dde53605130843aef907ba05986614e744c81
Robert Balicki <robert.balicki@gmail.com>
2025-12-20 20:48:05 -0800

    clippy

--- crates/common_lang_types/src/diagnostic.rs
+++ crates/common_lang_types/src/diagnostic.rs
@@ -69,7 +69,7 @@ impl std::fmt::Display for PrintableDiagnostic<'_> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "\n{}", self.diagnostic.0.message)?;
         if let Some(location) = self.diagnostic.location() {
-            write!(f, "\n")?;
+            writeln!(f)?;
             (self.print_location)(location, f)?;
         }
         Ok(())
