4b917332eef75dccc2dc33ca0da258b13226c0fc
Robert Balicki <robert.balicki@gmail.com>
2025-12-21 11:09:05 -0800

    Fix bug where when isograph is installed with Bun, the binary is not executable
    - Bun will run cli.js, but will fail at spawn(...)
    - So, we can chmod the binary in cli.js

--- libs/isograph-compiler/cli.js
+++ libs/isograph-compiler/cli.js
@@ -3,10 +3,18 @@
 
 var bin = require('.');
 var spawn = require('child_process').spawn;
+var chmodSync = require('fs').chmodSync;
 
 var input = process.argv.slice(2);
 
 if (bin != null) {
+  try {
+    // Force 755 (rwxr-xr-x)
+    //
+    // If Isograph is installed with Bun, the binary isn't always executable.
+    chmodSync(bin, 0o755);
+  } catch {}
+
   spawn(bin, input, { stdio: 'inherit' }).on('exit', process.exit);
 } else {
   throw new Error(
