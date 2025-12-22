56cde22b86ad0be5d75300782a9de543d1d847bf
Robert Balicki <robert.balicki@gmail.com>
2025-12-21 16:40:17 -0800

    move anothe rfile

--- crates/isograph_lang_types/src/declarations/mod.rs
+++ crates/isograph_lang_types/src/declarations/mod.rs
@@ -1,11 +1,13 @@
 mod client_selectable_declaration;
 mod entrypoint_declaration;
+mod isograph_type_annotation;
 mod selection_argument;
 mod selection_declaration;
 mod variable_declaration;
 
 pub use client_selectable_declaration::*;
 pub use entrypoint_declaration::*;
+pub use isograph_type_annotation::*;
 pub use selection_argument::*;
 pub use selection_declaration::*;
 pub use variable_declaration::*;
