3d4576949c78a483278b53b3675e815fb7cd715b
Robert Balicki <robert.balicki@gmail.com>
2025-12-21 23:25:55 -0800

    make VariableDeclarationInner generic over TLocation

--- crates/isograph_lang_types/src/declarations/variable_declaration.rs
+++ crates/isograph_lang_types/src/declarations/variable_declaration.rs
@@ -1,25 +1,25 @@
 use std::fmt::Debug;
 
-use common_lang_types::WithEmbeddedLocation;
+use common_lang_types::{EmbeddedLocation, WithGenericLocation};
 use resolve_position::PositionResolutionPath;
 use resolve_position_macros::ResolvePosition;
 
 use crate::{
-    ClientFieldDeclarationPath, ClientPointerDeclarationPath, ConstantValue, IsographResolvedNode,
-    TypeAnnotationDeclaration, VariableNameWrapper,
+    ClientFieldDeclarationPath, ClientPointerDeclarationPath, ConstantValueInner,
+    IsographResolvedNode, TypeAnnotationDeclaration, VariableNameWrapper,
 };
 
 #[derive(Clone, Eq, PartialEq, Ord, PartialOrd, Debug, Hash, ResolvePosition)]
-#[resolve_position(parent_type=VariableDeclarationParentType<'a>, resolved_node=IsographResolvedNode<'a>)]
-pub struct VariableDeclarationInner {
+#[resolve_position(parent_type=VariableDeclarationParentType<'a>, resolved_node=IsographResolvedNode<'a>, self_type_generics=<EmbeddedLocation>)]
+pub struct VariableDeclarationInner<TLocation> {
     #[resolve_field]
-    pub name: WithEmbeddedLocation<VariableNameWrapper>,
+    pub name: WithGenericLocation<VariableNameWrapper, TLocation>,
     #[resolve_field]
-    pub type_: WithEmbeddedLocation<TypeAnnotationDeclaration>,
-    pub default_value: Option<WithEmbeddedLocation<ConstantValue>>,
+    pub type_: WithGenericLocation<TypeAnnotationDeclaration, TLocation>,
+    pub default_value: Option<WithGenericLocation<ConstantValueInner<TLocation>, TLocation>>,
 }
 
-pub type VariableDeclaration = VariableDeclarationInner;
+pub type VariableDeclaration = VariableDeclarationInner<EmbeddedLocation>;
 
 pub type VariableDeclarationPath<'a> =
     PositionResolutionPath<&'a VariableDeclaration, VariableDeclarationParentType<'a>>;
