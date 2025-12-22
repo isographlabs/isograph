0d1c603caf779c63c0356b7692a079f6541b0e31
Robert Balicki <robert.balicki@gmail.com>
2025-12-21 16:30:24 -0800

    move declaration-related structs to their own folder

---
+++ crates/isograph_lang_types/src/declarations/mod.rs
@@ -0,0 +1,11 @@
+mod client_selectable_declaration;
+mod entrypoint_declaration;
+mod selection_argument;
+mod selection_declaration;
+mod variable_declaration;
+
+pub use client_selectable_declaration::*;
+pub use entrypoint_declaration::*;
+pub use selection_argument::*;
+pub use selection_declaration::*;
+pub use variable_declaration::*;
