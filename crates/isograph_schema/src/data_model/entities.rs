e69c5c7d7eab05ac490ebe32cb3bf550173545c1
Robert Balicki <robert.balicki@gmail.com>
2025-12-28 14:46:54 +0800

    remove outdated todo

--- crates/isograph_schema/src/data_model/entities.rs
+++ crates/isograph_schema/src/data_model/entities.rs
@@ -7,14 +7,11 @@ use crate::NetworkProtocol;
 #[derive(Debug, PartialEq, Eq, Clone, Hash, Copy)]
 pub struct IsConcrete(pub bool);
 
-// TODO Scalar and Object entities should be a single struct, with all the extra info
-// held in a network_protocol_associated_data field. (Some of that data, like the javascript_name,
-// is independent of the network protocol, but actually relates to the RuntimeLanguage, but
-// we do not (yet) have that as a generic.)
 #[derive(Debug, PartialEq, Eq, Clone, Hash)]
 pub struct ServerEntity<TNetworkProtocol: NetworkProtocol> {
     pub description: Option<Description>,
     pub name: EntityName,
+    // TODO this is obviously a hack
     pub selection_info: SelectionType<JavascriptName, IsConcrete>,
     pub network_protocol_associated_data: TNetworkProtocol::EntityAssociatedData,
 }
