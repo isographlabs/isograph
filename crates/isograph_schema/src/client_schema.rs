e49b1298c01084c507fcbd227ee7dc0b1e1e76c6
Robert Balicki <robert.balicki@gmail.com>
2026-01-18 15:39:26 -0500

    create entity for each client declaration

--- crates/isograph_schema/src/client_schema.rs
+++ crates/isograph_schema/src/client_schema.rs
@@ -1,9 +1,16 @@
+use common_lang_types::{DescriptionValue, EntityName, WithLocationPostfix};
+use intern::string_key::Intern;
+use isograph_lang_types::{
+    DefinitionLocationPostfix, Description, SelectionType, SelectionTypePostfix,
+};
 use pico_macros::memo;
 use prelude::Postfix;
 
 use crate::{
-    CompilationProfile, FlattenedDataModelSchema, IsographDatabase, NestedDataModelSchema,
-    flatten::Flatten,
+    CompilationProfile, FlattenedDataModelSchema, IsConcrete, IsographDatabase,
+    NestedDataModelEntity, NestedDataModelSchema, ServerObjectSelectionInfo,
+    client_selectable_declaration_map_from_iso_literals, flatten::Flatten,
+    insert_entity_into_schema_or_emit_multiple_definitions_diagnostic,
 };
 
 #[memo]
@@ -25,7 +32,88 @@ pub fn flattened_client_schema<TCompilationProfile: CompilationProfile>(
 // TODO return a schema that is flattened, but contains locations
 #[memo]
 pub fn nested_client_schema<TCompilationProfile: CompilationProfile>(
-    _db: &IsographDatabase<TCompilationProfile>,
+    db: &IsographDatabase<TCompilationProfile>,
 ) -> NestedDataModelSchema<TCompilationProfile> {
-    Default::default()
+    let mut schema = Default::default();
+
+    create_target_entity_for_each_client_declaration(db, &mut schema);
+
+    schema
+}
+
+pub fn create_target_entity_for_each_client_declaration<TCompilationProfile: CompilationProfile>(
+    db: &IsographDatabase<TCompilationProfile>,
+    schema: &mut NestedDataModelSchema<TCompilationProfile>,
+) {
+    let declarations = client_selectable_declaration_map_from_iso_literals(db);
+
+    for (_, declaration) in declarations.item.reference() {
+        match declaration.item {
+            SelectionType::Scalar(scalar_declaration) => {
+                let scalar_declaration = scalar_declaration.lookup(db);
+                insert_entity_into_schema_or_emit_multiple_definitions_diagnostic(
+                    schema,
+                    NestedDataModelEntity {
+                        name: format!(
+                            "{}__{}__targetEntity",
+                            scalar_declaration.parent_type.item,
+                            scalar_declaration.client_field_name.item
+                        )
+                        .intern()
+                        .to::<EntityName>()
+                        .with_missing_location(),
+                        description: format!(
+                            "The anonymous entity for the {}.{} client field",
+                            scalar_declaration.parent_type.item,
+                            scalar_declaration.client_field_name.item
+                        )
+                        .intern()
+                        .to::<DescriptionValue>()
+                        .to::<Description>()
+                        .with_missing_location()
+                        .wrap_some(),
+                        selectables: Default::default(),
+                        associated_data: ().client_defined(),
+                        selection_info: ().scalar_selected(),
+                    }
+                    .with_some_location(declaration.location),
+                );
+            }
+            SelectionType::Object(object_declaration) => {
+                let object_declaration = object_declaration.lookup(db);
+                insert_entity_into_schema_or_emit_multiple_definitions_diagnostic(
+                    schema,
+                    NestedDataModelEntity {
+                        name: format!(
+                            "{}__{}__targetEntity",
+                            object_declaration.parent_type.item,
+                            object_declaration.client_pointer_name.item
+                        )
+                        .intern()
+                        .to::<EntityName>()
+                        .with_missing_location(),
+                        description: format!(
+                            "The anonymous entity for the {}.{} client field",
+                            object_declaration.parent_type.item,
+                            object_declaration.client_pointer_name.item
+                        )
+                        .intern()
+                        .to::<DescriptionValue>()
+                        .to::<Description>()
+                        .with_missing_location()
+                        .wrap_some(),
+                        selectables: Default::default(),
+                        associated_data: ().client_defined(),
+
+                        selection_info: ServerObjectSelectionInfo {
+                            is_concrete: true.wrap(IsConcrete),
+                        }
+                        .note_todo("This is not a server object! This probably belongs on the associated_data")
+                        .object_selected(),
+                    }
+                    .with_some_location(declaration.location),
+                );
+            }
+        }
+    }
 }
