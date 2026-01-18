a04b497fdebebfa50a6f6bd36de6e046c4e4cbd0
Robert Balicki <robert.balicki@gmail.com>
2026-01-18 12:09:41 -0500

    flatten client schema from nested client schema

--- crates/isograph_schema/src/client_schema.rs
+++ crates/isograph_schema/src/client_schema.rs
@@ -1,12 +1,31 @@
-use std::collections::BTreeMap;
-
 use pico_macros::memo;
+use prelude::Postfix;
 
-use crate::{CompilationProfile, FlattenedDataModelSchema, IsographDatabase};
+use crate::{
+    CompilationProfile, FlattenedDataModelSchema, IsographDatabase, NestedDataModelSchema,
+    flatten::Flatten,
+};
 
 #[memo]
 pub fn flattened_client_schema<TCompilationProfile: CompilationProfile>(
-    _db: &IsographDatabase<TCompilationProfile>,
+    db: &IsographDatabase<TCompilationProfile>,
 ) -> FlattenedDataModelSchema<TCompilationProfile> {
-    BTreeMap::new()
+    nested_client_schema(db)
+        .item
+        .iter()
+        .map(|(key, value)| {
+            (
+                key.dereference(),
+                value.clone().drop_location().map(|x| x.flatten()),
+            )
+        })
+        .collect()
+}
+
+// TODO return a schema that is flattened, but contains locations
+#[memo]
+pub fn nested_client_schema<TCompilationProfile: CompilationProfile>(
+    _db: &IsographDatabase<TCompilationProfile>,
+) -> NestedDataModelSchema<TCompilationProfile> {
+    Default::default()
 }
