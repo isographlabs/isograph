f611e1579ca7bccdb5fa980c53c64897a31e6118
Robert Balicki <robert.balicki@gmail.com>
2026-01-19 23:40:19 -0500

    Insert client selectables for each client declaration, when generating the nested client schema

--- crates/isograph_schema/src/client_schema.rs
+++ crates/isograph_schema/src/client_schema.rs
@@ -1,15 +1,20 @@
-use common_lang_types::{DescriptionValue, EntityName, WithLocationPostfix};
+use common_lang_types::{
+    DescriptionValue, EntityName, Location, SelectableName, WithLocationPostfix,
+};
 use intern::string_key::Intern;
 use isograph_lang_types::{
-    DefinitionLocationPostfix, Description, SelectionType, SelectionTypePostfix,
+    DefinitionLocationPostfix, Description, EntityNameWrapper, SelectionType, SelectionTypePostfix,
+    TypeAnnotationDeclaration,
 };
 use pico_macros::memo;
 use prelude::Postfix;
 
 use crate::{
-    CompilationProfile, FlattenedDataModelSchema, IsographDatabase, NestedDataModelEntity,
-    NestedDataModelSchema, client_selectable_declaration_map_from_iso_literals, flatten::Flatten,
-    insert_entity_into_schema_or_emit_multiple_definitions_diagnostic,
+    CompilationProfile, FlattenedDataModelSchema, IsInlineFragment, IsographDatabase,
+    NestedDataModelEntity, NestedDataModelSchema, NestedDataModelSelectable,
+    client_selectable_declaration_map_from_iso_literals, entity_not_defined_diagnostic,
+    flatten::Flatten, insert_entity_into_schema_or_emit_multiple_definitions_diagnostic,
+    insert_selectable_into_schema_or_emit_multiple_definitions_diagnostic,
 };
 
 #[memo]
@@ -35,32 +40,34 @@ pub fn nested_client_schema<TCompilationProfile: CompilationProfile>(
 ) -> NestedDataModelSchema<TCompilationProfile> {
     let mut schema = Default::default();
 
-    create_target_entity_for_each_client_declaration(db, &mut schema);
+    insert_client_selectables_into_schema(db, &mut schema);
 
     schema
 }
 
-pub fn create_target_entity_for_each_client_declaration<TCompilationProfile: CompilationProfile>(
+pub fn insert_client_selectables_into_schema<TCompilationProfile: CompilationProfile>(
     db: &IsographDatabase<TCompilationProfile>,
     schema: &mut NestedDataModelSchema<TCompilationProfile>,
 ) {
-    let declarations = client_selectable_declaration_map_from_iso_literals(db);
-
-    for declaration in declarations.item.reference().values() {
+    for declaration in client_selectable_declaration_map_from_iso_literals(db)
+        .item
+        .reference()
+        .values()
+    {
         match declaration.item {
             SelectionType::Scalar(scalar_declaration) => {
                 let scalar_declaration = scalar_declaration.lookup(db);
+                let target_entity_name = format!(
+                    "{}__{}__targetEntity",
+                    scalar_declaration.parent_type.item, scalar_declaration.client_field_name.item
+                )
+                .intern()
+                .to::<EntityName>();
+
                 insert_entity_into_schema_or_emit_multiple_definitions_diagnostic(
                     schema,
                     NestedDataModelEntity {
-                        name: format!(
-                            "{}__{}__targetEntity",
-                            scalar_declaration.parent_type.item,
-                            scalar_declaration.client_field_name.item
-                        )
-                        .intern()
-                        .to::<EntityName>()
-                        .with_missing_location(),
+                        name: target_entity_name.with_missing_location(),
                         description: format!(
                             "The anonymous entity for the {}.{} client field",
                             scalar_declaration.parent_type.item,
@@ -77,8 +84,92 @@ pub fn create_target_entity_for_each_client_declaration<TCompilationProfile: Com
                     }
                     .with_some_location(declaration.location),
                 );
+
+                let Some(parent_entity) =
+                    schema.item.get_mut(&scalar_declaration.parent_type.item.0)
+                else {
+                    schema
+                        .non_fatal_diagnostics
+                        .push(entity_not_defined_diagnostic(
+                            scalar_declaration.parent_type.item.0,
+                            declaration.location.to::<Location>(),
+                        ));
+                    continue;
+                };
+
+                insert_selectable_into_schema_or_emit_multiple_definitions_diagnostic(
+                    &mut parent_entity.item.selectables,
+                    NestedDataModelSelectable {
+                        name: scalar_declaration
+                            .client_field_name
+                            .map_location(Some)
+                            .map(|x| x.0),
+                        parent_entity_name: scalar_declaration
+                            .parent_type
+                            .map_location(Some)
+                            .map(|x| x.0),
+                        description: scalar_declaration.description.map(|d| d.map_location(Some)),
+                        arguments: scalar_declaration
+                            .variable_definitions
+                            .clone()
+                            .into_iter()
+                            .map(|v| v.item)
+                            .collect(),
+                        target_entity: TypeAnnotationDeclaration::Scalar(
+                            target_entity_name.to::<EntityNameWrapper>(),
+                        )
+                        .wrap_ok()
+                        .with_missing_location(),
+                        associated_data: ().client_defined(),
+                        is_inline_fragment: IsInlineFragment(false),
+                    }
+                    .with_some_location(declaration.location),
+                );
             }
-            SelectionType::Object(_object_declaration) => {}
+            SelectionType::Object(object_declaration) => {
+                let object_declaration = object_declaration.lookup(db);
+
+                let Some(parent_entity) =
+                    schema.item.get_mut(&object_declaration.parent_type.item.0)
+                else {
+                    schema
+                        .non_fatal_diagnostics
+                        .push(entity_not_defined_diagnostic(
+                            object_declaration.parent_type.item.0,
+                            declaration.location.to::<Location>(),
+                        ));
+                    continue;
+                };
+
+                insert_selectable_into_schema_or_emit_multiple_definitions_diagnostic(
+                    &mut parent_entity.item.selectables,
+                    NestedDataModelSelectable {
+                        name: object_declaration
+                            .client_pointer_name
+                            .map_location(Some)
+                            .map(|x| x.0.to::<SelectableName>()),
+                        parent_entity_name: object_declaration
+                            .parent_type
+                            .map_location(Some)
+                            .map(|x| x.0),
+                        description: object_declaration.description.map(|d| d.map_location(Some)),
+                        arguments: object_declaration
+                            .variable_definitions
+                            .clone()
+                            .into_iter()
+                            .map(|v| v.item)
+                            .collect(),
+                        target_entity: object_declaration
+                            .target_type
+                            .clone()
+                            .map_location(Some)
+                            .map(Ok),
+                        associated_data: ().client_defined(),
+                        is_inline_fragment: IsInlineFragment(false),
+                    }
+                    .with_some_location(declaration.location),
+                );
+            }
         }
     }
 }
