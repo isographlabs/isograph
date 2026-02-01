9929b87895960f2bd396ea3d9f8ebd10071b5a42
PatrykWalach <35966385+PatrykWalach@users.noreply.github.com>
2026-02-01 12:00:31 +0100

    refactor(hmr): set displayName

--- libs/isograph-react/src/react/hmr.ts
+++ libs/isograph-react/src/react/hmr.ts
@@ -6,11 +6,25 @@
  *
  * This function adapts the isograph field signature to the standard React component signature.
  *
- * For HMR to work correctly, this has to be done at the top level of the module so that component is stable.
+ * For HMR to work correctly this has to be done:
+ * 1. at the top level of the module so that component is stable,
+ * 2. before HMR babel transform runs.
  */
-export function hmr<T, U, R>(
-  clientFieldResolver: (firstParameter: T, additionalRuntimeProps: U) => R,
-): (props: { firstParameter: T; additionalRuntimeProps: U }) => R {
-  return (props) =>
-    clientFieldResolver(props.firstParameter, props.additionalRuntimeProps);
+export function hmr<TReadFromStore, U, TClientFieldValue>(
+  clientFieldResolver: (
+    firstParameter: TReadFromStore,
+    additionalRuntimeProps: U,
+  ) => TClientFieldValue,
+) {
+  function Component(props: {
+    firstParameter: TReadFromStore;
+    additionalRuntimeProps: U;
+  }): TClientFieldValue {
+    return clientFieldResolver(
+      props.firstParameter,
+      props.additionalRuntimeProps,
+    );
+  }
+  Component.displayName = clientFieldResolver.name;
+  return Component;
 }
