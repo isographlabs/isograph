029114bb252af4cd807b2f119af4fd7d203de9bf
Robert Balicki <robert.balicki@gmail.com>
2026-08-22 00:08:48 -0400

    Allow unused IsographEvent::Quit.

--- crates/isograph_cli/src/event.rs
+++ crates/isograph_cli/src/event.rs
@@ -1,9 +1,12 @@
+#![expect(clippy::allow_attributes)]
+
 use std::path::PathBuf;
 
 #[derive(Debug, PartialEq, Eq, serde::Deserialize, serde::Serialize)]
 #[serde(tag = "kind", content = "value")]
 pub enum IsographEvent {
     HelloWorld,
+    #[allow(dead_code)]
     Quit,
     DiskChanged(DiskChanged),
 }
