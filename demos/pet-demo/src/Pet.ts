6e1e3bb2dd4a780cdfa927e5f0cb7207d5038ae6
Robert Balicki <robert.balicki@gmail.com>
2026-08-23 01:02:28 -0400

    tweak pet

--- demos/pet-demo/src/Pet.ts
+++ demos/pet-demo/src/Pet.ts
@@ -1,1 +1,8 @@
-export const fullName = iso(`field Pet.fullName { id }`);
+const iso: any = null;
+
+export const fullName = iso(`
+    field Pet.fullName @foo(bar: 123, baz: $qux)
+    {
+        id
+    }
+`);
