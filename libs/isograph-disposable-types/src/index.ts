ae4fea0ea2ff0826138c18780387237c8c4f42f1
Robert Balicki <robebalicki@twitter.com>
2026-09-16 12:32:45 -0400

    Add joinCleanups
    Takes ItemCleanupPairs and returns one pair of the item tuple and a
    cleanup that disposes them in reverse order.

--- libs/isograph-disposable-types/src/index.ts
+++ libs/isograph-disposable-types/src/index.ts
@@ -1,3 +1,28 @@
 export type CleanupFn = () => void;
 export type ItemCleanupPair<T> = [T, CleanupFn];
 export type Factory<T> = () => ItemCleanupPair<T>;
+
+type ItemsOf<T extends readonly ItemCleanupPair<unknown>[]> = {
+  [I in keyof T]: T[I] extends ItemCleanupPair<infer V> ? V : never;
+};
+
+export function joinCleanups<
+  const T extends readonly ItemCleanupPair<unknown>[],
+>(...pairs: T): ItemCleanupPair<ItemsOf<T>> {
+  const items = pairs.map(([item]) => item) as ItemsOf<T>;
+  const dispose: CleanupFn = () => {
+    let error: unknown;
+    pairs.reduceRight((_, [, cleanup]) => {
+      try {
+        cleanup();
+      } catch (e) {
+        error ??= e;
+      }
+      return undefined;
+    }, undefined);
+    if (error !== undefined) {
+      throw error;
+    }
+  };
+  return [items, dispose];
+}
